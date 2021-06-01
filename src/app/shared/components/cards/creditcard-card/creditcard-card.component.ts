ac06c430ec27bed628c86d9c26e8707fce720827
jonathanbravecredit <jonathan@brave.credit>
2021-06-01 00:01:43 -0400

    save

--- src/app/shared/components/cards/creditcard-card/creditcard-card.component.ts
+++ src/app/shared/components/cards/creditcard-card/creditcard-card.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ViewdetailButtonComponent } from '@shared/components/buttons/viewdetail-button/viewdetail-button.component';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'brave-creditcard-card',
   templateUrl: './creditcard-card.component.html',
 })
-export class CreditcardCardComponent implements OnInit {
+export class CreditcardCardComponent implements AfterViewInit {
+  @ViewChild(ViewdetailButtonComponent)
+  viewDetail: ViewdetailButtonComponent | undefined;
+  open$: Observable<boolean> | undefined = of(false);
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.open$ = this.viewDetail?.open$.asObservable();
+    this.open$?.subscribe((val) => console.log(val));
+  }
 }
