d11044f4136e14cd5ca83121ffa2a9bb8ec510f4
jonathanbravecredit <jonathan@brave.credit>
2021-06-07 20:51:01 -0700

    rebuilding storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,4 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
 module.exports = {
-	webpackFinal: async (config, { configType }) => {
-    const shouldAnalyze = process.env.analyze === "true";
-        if (shouldAnalyze) {
-            config.plugins.push(
-                new BundleAnalyzerPlugin()
-            );
-        }
-
-        config.optimization = {
-            splitChunks: {
-                chunks: "all",
-                minSize: 30 * 1024, // 30KB
-                maxSize: 1024 * 1024, // 1MB
-            }
-        };
-
-        return config;
-  },
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
