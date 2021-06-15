81913dfa2bda1e5674b71ba121028f763959f1c4
jonathanbravecredit <jonathan@brave.credit>
2021-06-15 16:34:51 -0700

    WIP BL for negative accounts
    moved nav to layout
    moved negative cards to input

--- src/app/views/negative-account/negative-account-initial-pure/negative-account-initial-pure.component.ts
+++ src/app/views/negative-account/negative-account-initial-pure/negative-account-initial-pure.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { INegativeAccountCardInputs } from '@shared/components/cards/negative-account-card/negative-account-card.component';
 
 @Component({
   selector: 'brave-negative-account-initial-pure',
   templateUrl: './negative-account-initial-pure.component.html',
 })
 export class NegativeAccountInitialPureComponent implements OnInit {
+  @Input() cards: INegativeAccountCardInputs[] | undefined;
+
   constructor() {}
 
   ngOnInit(): void {}
