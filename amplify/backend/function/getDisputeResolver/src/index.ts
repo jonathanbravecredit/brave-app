51b856f68498322a42d8675ca84ff32e7098f26d
jonathanbravecredit <jonathan@brave.credit>
2021-06-25 11:55:20 -0700

    updated resolver

--- amplify/backend/function/getDisputeResolver/src/index.ts
+++ amplify/backend/function/getDisputeResolver/src/index.ts
@@ -5,30 +5,39 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+import { IResolverEvent } from '@lib/interfaces/resolver.interface';
 import { getDispute } from '@lib/queries/getDisputes';
-import { AppSyncResolverEvent } from 'aws-lambda';
 
+/**
+ * Using this as the entry point, you can use a single function to handle many resolvers.
+ */
 const resolvers = {
   Query: {
-    getDisputes: (id: string) => {
-      return getDispute(id);
+    getDisputes: async (id: string) => {
+      return await getDispute(id);
     },
   },
 };
 
-export const handler: any = async (event: AppSyncResolverEvent<any>) => {
-  console.log('evet ===> ', event);
-
-  // const typeHandler = resolvers[event.typeName];
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+/**
+ *
+ * @param {string} typeName ex: Query - Filled dynamically based on function usage location
+ * @param {string} fieldName ex: getUser - Filled dynamically based on function usage location. Typically name of query
+ * @param {Object} arguments GraphQL field arguments via $ctx.arguments
+ * @param {Object} identity AppSync identity object via $ctx.identity
+ * @param {Object} source The object returned by the parent resolver. E.G. if resolving field 'Post.comments', the source is the Post object.
+ * @param {Object} request AppSync request object. Contains things like headers.
+ * @param {Object} prev If using the built-in pipeline resolver support, this contains the object returned by the previous function.
+ * @returns
+ */
+export const handler: any = async (event: IResolverEvent) => {
+  console.log('event ===> ', event);
+  const typeHandler = resolvers[event.typeName];
+  if (typeHandler) {
+    const resolver = typeHandler[event.fieldName];
+    if (resolver) {
+      return await resolver(event);
+    }
+  }
+  throw new Error('Resolver not found.');
 };
