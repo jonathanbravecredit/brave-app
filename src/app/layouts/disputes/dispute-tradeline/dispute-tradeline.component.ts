eebe62815a6beefd14cbda839ab05e4deb55221a
Jo Carrasco <jose.contractor@brave.credit>
2021-06-30 20:22:01 -0400

    WIP Setting up tradeline view

---
+++ src/app/layouts/disputes/dispute-tradeline/dispute-tradeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'brave-dispute-tradeline',
+  templateUrl: './dispute-tradeline.component.html',
+  styleUrls: ['./dispute-tradeline.component.css']
+})
+export class DisputeTradelineTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
