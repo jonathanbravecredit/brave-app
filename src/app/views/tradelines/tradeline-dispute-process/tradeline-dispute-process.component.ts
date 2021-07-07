492ef19c3b1f9e5b2e6af564635bf442f34e609a
jonathanbravecredit <jonathan@brave.credit>
2021-07-07 14:12:11 -0700

    fixing build errors

--- src/app/views/tradelines/tradeline-dispute-process/tradeline-dispute-process.component.ts
+++ src/app/views/tradelines/tradeline-dispute-process/tradeline-dispute-process.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { INegativeAccountCardInputs } from '@shared/components/cards/negative-account-card/negative-account-card.component';
+import { INegativeAccountCardInputs } from '@shared/components/cards/negative-account-card/interfaces';
 import { IDisputeTradelineProcessResult } from '@shared/components/tradelines/tradeline-dispute-process/interfaces';
 import { TradelineDisputeProcessComponent } from '@shared/components/tradelines/tradeline-dispute-process/tradeline-dispute-process.component';
-
 
 @Component({
   selector: 'brave-tradeline-dispute-process-view',
   templateUrl: './tradeline-dispute-process.component.html',
-  styleUrls: ['./tradeline-dispute-process.component.css']
+  styleUrls: ['./tradeline-dispute-process.component.css'],
 })
 export class TradelineDisputeProcessView implements OnInit {
   @ViewChild(TradelineDisputeProcessComponent) tradelineDisputeProcess: TradelineDisputeProcessComponent | undefined;
@@ -16,8 +15,8 @@ export class TradelineDisputeProcessView implements OnInit {
   @Input() initialStepId = 'select';
   @Input() forceNavigation = false;
   @Input() initialDisputeType: string | undefined = undefined;
-  
-  negativeAccountCardData: INegativeAccountCardInputs = {
+
+  negativeAccountCardData = {
     creditorName: 'H.J National Collections',
     lastReported: '05/15/21',
     originalCreditor: 'Original Creditor',
@@ -27,13 +26,12 @@ export class TradelineDisputeProcessView implements OnInit {
       typeOfCollection: 'Collections',
       amountPastDue: 700,
       dateOpened: '04/12/2018',
-      dateLastPayment: '04/21/2018'
-    }
-  };
-  constructor() { }
+      dateLastPayment: '04/21/2018',
+    },
+  } as INegativeAccountCardInputs;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   requestGoBack() {
     const currentInnerProcessNavigationIndex = this.tradelineDisputeProcess?.getCurrentNavigationIndex();
@@ -41,11 +39,11 @@ export class TradelineDisputeProcessView implements OnInit {
       if (currentInnerProcessNavigationIndex > 0) {
         this.tradelineDisputeProcess?.goBack();
       }
-    } 
+    }
   }
 
   onDisputeProcessResult(result: IDisputeTradelineProcessResult): void {
-    // result event has a data property where the reason ids can be pull out and find them in the constants of the tradeline component 
+    // result event has a data property where the reason ids can be pull out and find them in the constants of the tradeline component
     if (result.isFinished) {
       this.isDisputeSent = true;
       this.isDisputeProcessInProgress = false;
