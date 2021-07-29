5c2172071eca03cd847865751b6cbcea504252d5
Jonathan <jonathan@brave.credit>
2021-07-29 08:24:32 -0700

    save

--- src/app/views/dashboard-init/dashboard-init/dashboard-init.component.ts
+++ src/app/views/dashboard-init/dashboard-init/dashboard-init.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { SnapshotStatus } from '@shared/components/cards/snapshot-display-card/snapshot-display-card.component';
-import { LabelOfSnapshot } from '@shared/components/cards/snapshot-display-card/snapshot-label.pipe';
 
 @Component({
   selector: 'brave-dashboard-init',
   templateUrl: './dashboard-init.component.html',
 })
 export class DashboardInitComponent {
+  isEnrolled: boolean = true;
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   goToReport() {
