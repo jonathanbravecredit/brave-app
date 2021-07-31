6ac398fe739e5dbee043473d3408c4e7ab454804
Jonathan <jonathan@brave.credit>
2021-07-31 16:02:02 -0700

    adding api and auth to prod env

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,12 +7,26 @@ const awsmobile = {
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_R22DawoAR",
     "aws_user_pools_web_client_id": "rpqo3q7uaftlr8rp39kfvh08t",
-    "oauth": {},
+    "oauth": {
+        "domain": "braveappb029d416-b029d416-prod.auth.us-east-2.amazoncognito.com",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "http://localhost:4200/auth/redirect/,https://app.brave.credit/auth/redirect/",
+        "redirectSignOut": "http://localhost:4200/auth/signin/,https://app.brave.credit/auth/signin/",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
     "aws_appsync_graphqlEndpoint": "https://krybm5pc3nbgdk4zwgcva2i4va.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
     "aws_user_files_s3_bucket": "braveapp7123270838f144d09004f496c2f9ef0f115121-prod",
     "aws_user_files_s3_bucket_region": "us-east-2"
 };
+
 
 export default awsmobile;
