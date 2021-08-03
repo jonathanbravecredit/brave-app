adc957f0e70bef37141ed8ebd42adbd7ec1962e2
JoCarrasco <jose.contractor@brave.credit>
2021-08-03 11:45:49 -0400

    feat(online-disputes-terms): Change terms constants

--- src/app/shared/components/modals/onboarding-dispute/constants.ts
+++ src/app/shared/components/modals/onboarding-dispute/constants.ts
@@ -5,12 +5,12 @@ export const REFRESH_TERMS = [
 ];
 
 export const ONLINE_DISPUTE_TERMS = [
-  `As part of the dispute process, TransUnion may contact the data furnisher that reported the information being disputed.`,
+  `As part of the dispute process, TransUnion® may contact the data furnisher that reported the information being disputed. `,
   `Any disputed information that is verified as accurate will remain on the credit report.`,
-  `The dispute options provided through this dispute service do not include “all” possible dispute options; consumers need to raise their dispute directly through TransUnion instead if they wish to update the SSN, date of birth, phone number, current name or address, or want to add new name or address information to their TransUnion credit report.`,
-  `Consumer are unable to provide supporting documentation through this Dispute Service. In the event the consumer wants to submit supporting documentation, consumer should submit their dispute to TransUnion directly.`,
-  `Consumer agrees to be notified via email when the dispute is complete, and agrees to log into this site to access their Investigation results.`,
-  `I am requesting an investigation based on my own TransUnion credit report information.`,
-  `I understand that Brave Credit itself does not process dispute requests and is not responsible for investigating or correcting errors in your credit report.`,
-  `I authorize Caboodle Corp dba Brave Credit to transmit my dispute request to TransUnion and to accept the results of my dispute investigation electronically on my behalf in lieu of postal mail delivery.`,
+  `The dispute options provided through this dispute service do not include “all” possible dispute options; consumers need to raise their dispute directly through TransUnion® instead if they wish to update the SSN, date of birth, phone number, current name or address, or want to add new name or address information to their TransUnion® credit report.`,
+  `Consumers are unable to provide supporting documentation through this Dispute Service. In the event the consumer wants to submit supporting documentation, consumer should submit their dispute to TransUnion® directly.`,
+  `Consumer agrees to be notified via email when the dispute is complete, and agrees to log into this site to access their Investigation results`,
+  `I am requesting an investigation based on my own TransUnion® credit report information`,
+  `I understand that Brave Credit itself does not process dispute requests and is not responsible for investigating or correcting errors in your credit report`,
+  `I authorize Caboodle Corp dba Brave Credit to transmit my dispute request to TransUnion® and to accept the results of my dispute investigation electronically on my behalf in lieu of postal mail delivery`,
 ];
