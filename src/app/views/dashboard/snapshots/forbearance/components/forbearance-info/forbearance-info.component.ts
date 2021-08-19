29ff33657799ec821097a6beca21b154e6f3f8bd
Jonathan <jonathan@brave.credit>
2021-08-19 12:27:04 -0600

    header and info comp complete

--- src/app/views/dashboard/snapshots/forbearance/components/forbearance-info/forbearance-info.component.ts
+++ src/app/views/dashboard/snapshots/forbearance/components/forbearance-info/forbearance-info.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'brave-forbearance-info',
   templateUrl: './forbearance-info.component.html',
 })
 export class ForbearanceInfoComponent implements OnInit {
+  @Output() clickedInfo: EventEmitter<void> = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
