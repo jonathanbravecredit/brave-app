00a25176257748c5dc4ba208ca578b7d0c973fc8
Jonathan <jonathan@brave.credit>
2021-08-25 17:20:05 -0700

    save

--- src/app/views/authentication/signin-redirect/signin-redirect.component.ts
+++ src/app/views/authentication/signin-redirect/signin-redirect.component.ts
@@ -20,19 +20,17 @@ export class SigninRedirectComponent implements OnInit {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    // try {
-    //   // const creds: ICredentials = await Auth.currentUserCredentials();
-    //   const creds: CognitoUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
-    //   console.log('creds ===> ', creds);
-    //   const attrs = await Auth.userAttributes(creds);
-    //   const id = attrs.filter((a) => a.Name === 'sub')[0]?.Value;
-    //   await this.sync.onboardUser(id, false);
-    //   this.interstitial.closeInterstitial();
-    // } catch (err) {
-    //   this.router.navigate(['/auth/invalid']);
-    //   console.log('hall monitor error', err);
-    // }
+    try {
+      // const creds: ICredentials = await Auth.currentUserCredentials();
+      const creds: CognitoUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
+      console.log('creds ===> ', creds);
+      const attrs = await Auth.userAttributes(creds);
+      const id = attrs.filter((a) => a.Name === 'sub')[0]?.Value;
+      await this.sync.onboardUser(id, false);
+      this.interstitial.closeInterstitial();
+    } catch (err) {
+      this.router.navigate(['/auth/invalid']);
+      console.log('hall monitor error', err);
+    }
   }
 }
-
-const navs: Record<string, any> = {};
