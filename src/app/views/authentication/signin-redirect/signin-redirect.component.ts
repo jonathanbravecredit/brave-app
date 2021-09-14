e918aae56973b9ce3d8ca91f7d57101ac36fd6d7
Jonathan <jonathan@brave.credit>
2021-09-13 18:31:17 -0700

    for debugging

--- src/app/views/authentication/signin-redirect/signin-redirect.component.ts
+++ src/app/views/authentication/signin-redirect/signin-redirect.component.ts
@@ -23,33 +23,33 @@ export class SigninRedirectComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<void> {
-    try {
-      const creds: CognitoUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
-      const attrs = await Auth.userAttributes(creds);
-      const id = attrs.filter((a) => a.Name === 'sub')[0]?.Value;
-      const isNew = await this.sync.isUserBrandNew(id);
-      if (isNew) {
-        this.cleanUp();
-        this.router.navigate(['/auth/created']);
-      } else {
-        await this.sync.initUser(id);
-        await this.sync.subscribeToListeners(id);
-        await this.sync.onboardUser(id, true);
-        this.cleanUp();
-      }
-    } catch (err) {
-      const provider = window.sessionStorage.getItem('braveOAuthProvider') as CognitoHostedUIIdentityProvider;
-      if (provider) {
-        await this.auth.socialSignIn(provider);
-      } else {
-        this.router.navigate(['/auth/invalid']);
-        this.cleanUp();
-      }
-    }
+    // try {
+    //   const creds: CognitoUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
+    //   const attrs = await Auth.userAttributes(creds);
+    //   const id = attrs.filter((a) => a.Name === 'sub')[0]?.Value;
+    //   const isNew = await this.sync.isUserBrandNew(id);
+    //   if (isNew) {
+    //     this.cleanUp();
+    //     this.router.navigate(['/auth/created']);
+    //   } else {
+    //     await this.sync.initUser(id);
+    //     await this.sync.subscribeToListeners(id);
+    //     await this.sync.onboardUser(id, true);
+    //     this.cleanUp();
+    //   }
+    // } catch (err) {
+    //   const provider = window.sessionStorage.getItem('braveOAuthProvider') as CognitoHostedUIIdentityProvider;
+    //   if (provider) {
+    //     await this.auth.socialSignIn(provider);
+    //   } else {
+    //     this.router.navigate(['/auth/invalid']);
+    //     this.cleanUp();
+    //   }
+    // }
   }
 
   cleanUp(): void {
-    window.sessionStorage.removeItem('braveOAuthProvider');
-    this.interstitial.closeInterstitial();
+    // window.sessionStorage.removeItem('braveOAuthProvider');
+    // this.interstitial.closeInterstitial();
   }
 }
