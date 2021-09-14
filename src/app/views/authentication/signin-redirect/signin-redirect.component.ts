b06649e879cfd9c7389e2518d33ccde2b54a9966
Jonathan <jonathan@brave.credit>
2021-09-14 10:17:29 -0700

    redirect race condition
    waiting until app is stable before redirecting
    potential race condition with auth guard which is returning false

--- src/app/views/authentication/signin-redirect/signin-redirect.component.ts
+++ src/app/views/authentication/signin-redirect/signin-redirect.component.ts
@@ -1,28 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnDestroy } from '@angular/core';
 import { AuthService } from '@shared/services/auth/auth.service';
 import { SyncService } from '@shared/services/sync/sync.service';
 import { InterstitialService } from '@shared/services/interstitial/interstitial.service';
 import { Router } from '@angular/router';
 import Auth, { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 import { CognitoUser } from 'amazon-cognito-identity-js';
+import { Subscription } from 'rxjs';
+import { first, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'brave-signin-redirect',
   templateUrl: './signin-redirect.component.html',
 })
-export class SigninRedirectComponent implements OnInit {
+export class SigninRedirectComponent implements OnDestroy {
   provider = CognitoHostedUIIdentityProvider;
+  appSub$: Subscription;
   constructor(
     private router: Router,
     private sync: SyncService,
     private auth: AuthService,
+    private appRef: ApplicationRef,
     private interstitial: InterstitialService,
   ) {
     this.interstitial.changeMessage(' ');
     this.interstitial.openInterstitial();
+    this.appSub$ = this.appRef.isStable
+      .pipe(
+        first((stable) => stable),
+        tap(async (stable) => {
+          await this.onboardUser();
+        }),
+      )
+      .subscribe();
   }
 
-  async ngOnInit(): Promise<void> {
+  ngOnDestroy(): void {
+    this.appSub$.unsubscribe();
+  }
+
+  async onboardUser(): Promise<void> {
     try {
       const creds: CognitoUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
       const attrs = await Auth.userAttributes(creds);
