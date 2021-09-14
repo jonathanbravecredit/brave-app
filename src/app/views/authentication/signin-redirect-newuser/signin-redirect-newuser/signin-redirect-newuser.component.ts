f8f139f9084cf4ba85fc101862310c4e08c2da50
Jonathan <jonathan@brave.credit>
2021-09-13 20:17:30 -0700

    redirect race condition
    waiting until app is stable before redirecting
    potential race condition with auth guard which is returning false

--- src/app/views/authentication/signin-redirect-newuser/signin-redirect-newuser/signin-redirect-newuser.component.ts
+++ src/app/views/authentication/signin-redirect-newuser/signin-redirect-newuser/signin-redirect-newuser.component.ts
@@ -17,7 +17,6 @@ export class SigninRedirectNewuserComponent implements OnDestroy {
       .pipe(
         first((stable) => stable),
         tap((stable) => {
-          console.log('stable now');
           setTimeout(async () => {
             await this.onboardUser();
           }, 4500);
