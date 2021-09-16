333e3d44615e1e61b57ef5803e108cbb5f831f56
Jonathan <jonathan@brave.credit>
2021-09-15 20:37:27 -0700

    credit report updates
    open vs closed tradelines
    max delinquency
    student loans forebearance
    alphabetical order of states
    address form
    fraud text

--- src/app/views/dashboard/snapshots/data-breaches/components/data-breach-card/content.ts
+++ src/app/views/dashboard/snapshots/data-breaches/components/data-breach-card/content.ts
@@ -3,6 +3,6 @@ export const dataBreachCardContent = {
   link1: `review your credit report`,
   p2: `for accounts you don’t recognize on Brave Credit. If you see something you don’t recognize you can,`,
   link2: `dispute the information`,
-  P3: `or put a credit freeze on your credit report for all bureaus`,
+  p3: `or put a credit freeze on your credit report for all bureaus`,
   b1: `See more`,
 };
