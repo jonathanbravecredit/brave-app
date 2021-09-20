8f0b21724a9c3e1ae099fc1a9202504e96272cf7
Jonathan <jonathan@brave.credit>
2021-09-20 12:23:27 -0700

    adding app level defaults

--- src/app/shared/utils/brave/brave-base.ts
+++ src/app/shared/utils/brave/brave-base.ts
@@ -1,3 +1,9 @@
 export class BraveBase {
+  static authAttempts = 2;
+  static pinAttempts = 3;
+  static pinRequests = 3;
+  static pinMaxAge = 15 * 60 * 1000;
+  static kbaAttempts = 1;
+  static kbaMaxAge = 96 * 60 * 1000;
   constructor() {}
 }
