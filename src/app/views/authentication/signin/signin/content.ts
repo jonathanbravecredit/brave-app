d0fb72f659f52a2749f783c5662434156cab78a7
Jonathan <jonathan@brave.credit>
2021-09-24 09:55:28 -0700

    sign in refactor
    updated not authorized exception

--- src/app/views/authentication/signin/signin/content.ts
+++ src/app/views/authentication/signin/signin/content.ts
@@ -8,6 +8,6 @@ export enum SignInErrors {
 export const SignInErrorDescriptions: Record<any, string> = {
   [SignInErrors.UserNotConfirmedException]: `To log in please confirm your account by clicking on the button in the email we sent you`,
   [SignInErrors.PasswordResetRequiredException]: `Password reset required`,
-  [SignInErrors.NotAuthorizedException]: ``,
+  [SignInErrors.NotAuthorizedException]: `Not authorized`,
   [SignInErrors.PasswordResetRequiredException]: `Please use a registered email`,
 };
