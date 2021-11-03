7fb52f090a5810418b3d45674fae61a620252dde
Jonathan <jonathan@brave.credit>
2021-11-03 13:41:55 -0700

    WIP

--- src/app/views/dashboard/disputes/disputes-historical/disputes-historical-pure/disputes-historical-pure.view.ts
+++ src/app/views/dashboard/disputes/disputes-historical/disputes-historical-pure/disputes-historical-pure.view.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'brave-disputes-historical-pure',
   templateUrl: './disputes-historical-pure.view.html',
-  styleUrls: ['./disputes-historical-pure.view.css']
 })
 export class DisputesHistoricalPureView implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/views/dashboard/disputes/disputes-historical/disputes-historical/disputes-historical.view.ts
+++ src/app/views/dashboard/disputes/disputes-historical/disputes-historical/disputes-historical.view.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'brave-disputes-historical',
   templateUrl: './disputes-historical.view.html',
-  styleUrls: ['./disputes-historical.view.css']
 })
 export class DisputesHistoricalView implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
