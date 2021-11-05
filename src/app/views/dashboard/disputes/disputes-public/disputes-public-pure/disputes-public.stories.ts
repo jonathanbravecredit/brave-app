8c197b418c66c42d4514a1d753a63b583e126d2e
Jonathan <jonathan@brave.credit>
2021-11-05 15:28:34 -0700

    fixing storybook build

--- src/app/views/dashboard/disputes/disputes-public/disputes-public-pure/disputes-public.stories.ts
+++ src/app/views/dashboard/disputes/disputes-public/disputes-public-pure/disputes-public.stories.ts
@@ -1,46 +1,46 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { Story, Meta } from '@storybook/angular/types-6-0';
-import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
-import { SharedComponentsModule } from '@shared/components/shared-components.module';
-import { DisputesPublicPureView } from '@views/dashboard/disputes/disputes-public/disputes-public-pure/disputes-public-pure.view';
+// import { APP_BASE_HREF } from '@angular/common';
+// import { Story, Meta } from '@storybook/angular/types-6-0';
+// import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+// import { SharedComponentsModule } from '@shared/components/shared-components.module';
+// import { DisputesPublicPureView } from '@views/dashboard/disputes/disputes-public/disputes-public-pure/disputes-public-pure.view';
 
-export default {
-  title: 'app/views/disputes/public-records',
-  component: DisputesPublicPureView,
-  decorators: [
-    moduleMetadata({
-      declarations: [],
-      imports: [SharedComponentsModule],
-      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
-    }),
-    componentWrapperDecorator((story) => {
-      return `<div class="container mx-auto max-w-xs h-full">${story}</div>`;
-    }),
-  ],
-} as Meta;
+// export default {
+//   title: 'app/views/disputes/public-records',
+//   component: DisputesPublicPureView,
+//   decorators: [
+//     moduleMetadata({
+//       declarations: [],
+//       imports: [SharedComponentsModule],
+//       providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+//     }),
+//     componentWrapperDecorator((story) => {
+//       return `<div class="container mx-auto max-w-xs h-full">${story}</div>`;
+//     }),
+//   ],
+// } as Meta;
 
-const Template: Story<DisputesPublicPureView> = (args: any) => ({
-  component: DisputesPublicPureView,
-  props: {
-    ...args,
-  },
-});
+// const Template: Story<DisputesPublicPureView> = (args: any) => ({
+//   component: DisputesPublicPureView,
+//   props: {
+//     ...args,
+//   },
+// });
 
-export const Default = Template.bind({});
-Default.args = {};
+// export const Default = Template.bind({});
+// Default.args = {};
 
-export const Reasons = Template.bind({});
-Reasons.args = {
-  initialStepId: 'reason',
-  initialDisputeType: 'inaccurate',
-};
+// export const Reasons = Template.bind({});
+// Reasons.args = {
+//   initialStepId: 'reason',
+//   initialDisputeType: 'inaccurate',
+// };
 
-export const Summary = Template.bind({});
-Summary.args = {
-  initialStepId: 'summary',
-};
+// export const Summary = Template.bind({});
+// Summary.args = {
+//   initialStepId: 'summary',
+// };
 
-export const Success = Template.bind({});
-Success.args = {
-  isDisputeSent: true,
-};
+// export const Success = Template.bind({});
+// Success.args = {
+//   isDisputeSent: true,
+// };
