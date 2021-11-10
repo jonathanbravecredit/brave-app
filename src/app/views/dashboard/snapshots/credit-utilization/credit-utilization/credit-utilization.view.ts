a05b181171447c860b0a6ebb316ee31b46f56f1f
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-10 14:36:20 -0800

    small formating fix

--- src/app/views/dashboard/snapshots/credit-utilization/credit-utilization/credit-utilization.view.ts
+++ src/app/views/dashboard/snapshots/credit-utilization/credit-utilization/credit-utilization.view.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Store } from "@ngxs/store";
+import { IMergeReport } from "@shared/interfaces";
+import { AnalyticsService } from "@shared/services/analytics/analytics/analytics.service";
+import { CreditreportService } from "@shared/services/creditreport/creditreport.service";
+import { Observable } from "rxjs";
+import { ICreditUtilization } from "../components/credit-utilization-card/interfaces";
 
 @Component({
-  selector: 'brave-credit-utilization',
-  templateUrl: './credit-utilization.view.html',
-  styleUrls: ['./credit-utilization.view.css']
+  selector: "brave-credit-utilization",
+  templateUrl: "./credit-utilization.view.html",
+  styleUrls: ["./credit-utilization.view.css"],
 })
 export class CreditUtilizationView implements OnInit {
+  creditReport$: Observable<IMergeReport>;
 
-  constructor() { }
+  creditAcounts: ICreditUtilization[] = []
 
-  ngOnInit(): void {
+  constructor(
+    private creditReportService: CreditreportService,
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute,
+    private analytics: AnalyticsService
+  ) {
+    this.creditReport$ = this.creditReportService.tuReport$.asObservable();
   }
 
+  ngOnInit(): void {}
 }
