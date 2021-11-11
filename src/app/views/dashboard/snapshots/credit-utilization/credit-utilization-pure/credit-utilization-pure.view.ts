63502ae88135253b4ddf9f15a3ce2727a4bc4f2f
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-10 17:11:57 -0800

    added calcs for total credit debt and percentage

--- src/app/views/dashboard/snapshots/credit-utilization/credit-utilization-pure/credit-utilization-pure.view.ts
+++ src/app/views/dashboard/snapshots/credit-utilization/credit-utilization-pure/credit-utilization-pure.view.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ITradeLinePartition } from '@shared/interfaces/merge-report.interface';
-import { ITradelineDetailsConfig } from '@views/dashboard/reports/credit-report/tradelines/components/tradeline-details/interfaces';
-import { ICreditUtilization } from '@views/dashboard/snapshots/credit-utilization/components/credit-utilization-card/interfaces';
+import { Component, Input, OnInit } from "@angular/core";
+import { ITradeLinePartition } from "@shared/interfaces/merge-report.interface";
+import { ITradelineDetailsConfig } from "@views/dashboard/reports/credit-report/tradelines/components/tradeline-details/interfaces";
+import { ICreditUtilization } from "@views/dashboard/snapshots/credit-utilization/components/credit-utilization-card/interfaces";
 
 @Component({
-  selector: 'brave-credit-utilization-pure',
-  templateUrl: './credit-utilization-pure.view.html',
-  styleUrls: ['./credit-utilization-pure.view.css'],
+  selector: "brave-credit-utilization-pure",
+  templateUrl: "./credit-utilization-pure.view.html",
+  styleUrls: ["./credit-utilization-pure.view.css"],
 })
 export class CreditUtilizationPureView implements OnInit {
   @Input() creditAcounts: ITradeLinePartition[] = [];
@@ -25,20 +25,33 @@ export class CreditUtilizationPureView implements OnInit {
     if (this.hasCards) {
       this.debtAmount = this.sumDebtAmount(this.creditAcounts);
       this.totalAmount = this.sumTotalAmount(this.creditAcounts);
-      this.utilizationPerc = this.calcUtilzationPerc(this.debtAmount, this.totalAmount);
+      this.utilizationPerc = this.calcUtilzationPerc(
+        this.debtAmount,
+        this.totalAmount
+      );
     }
   }
 
   sumDebtAmount(account: ITradeLinePartition[]): number {
-    return 1000;
+    return account.reduce<number>(
+      (acc: number, tradePart: ITradeLinePartition) => {
+        return acc + +tradePart.Tradeline?.currentBalance!;
+      },
+      0
+    );
   }
 
   sumTotalAmount(account: ITradeLinePartition[]): number {
-    return 2000;
+    return account.reduce<number>(
+      (acc: number, tradePart: ITradeLinePartition) => {
+        return acc + +tradePart.Tradeline?.GrantedTrade.CreditLimit!;
+      },
+      0
+    );
   }
 
   calcUtilzationPerc(debt: number, total: number): number {
     if (total === 0) return 0;
-    return (debt / total) * 100;
+    return Math.floor((debt / total) * 100);
   }
 }
