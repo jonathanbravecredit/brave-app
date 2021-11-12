b034f21cb0281e1234741a342879618e181c3b43
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-12 11:26:48 -0800

    removal of NGstyles for tailwind classes

--- src/app/views/dashboard/snapshots/credit-utilization/credit-utilization-pure/credit-utilization-pure.view.ts
+++ src/app/views/dashboard/snapshots/credit-utilization/credit-utilization-pure/credit-utilization-pure.view.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ITradeLinePartition } from "@shared/interfaces/merge-report.interface";
 
 @Component({
