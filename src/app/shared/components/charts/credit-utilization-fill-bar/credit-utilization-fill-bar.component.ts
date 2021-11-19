bb23a60c99efc61d5a1a517faf0fcbf2d3591cde
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-18 17:24:25 -0800

    finishing up the credit util bar graph

--- src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
+++ src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
@@ -1,25 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'brave-credit-utilization-fill-bar',
-  templateUrl: './credit-utilization-fill-bar.component.html'
+  selector: "brave-credit-utilization-fill-bar",
+  templateUrl: "./credit-utilization-fill-bar.component.html",
 })
 export class CreditUtilizationFillBarComponent implements OnInit {
-
   @Input() creditType: string | undefined;
   @Input() maxCreditAmount: string | number | undefined;
   @Input() currentBalance: string | number | undefined;
+  @Input() highestBalance: string | number | undefined;
+  @Input() openClosed: string | undefined
   utilPercentage: string | undefined;
-  isLoan: boolean = false
+  isLoan: boolean = false;
+  isOpen: boolean = false
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.isLoan = this.creditType === 'c' || this.creditType === 'i' || this.creditType === 'm'
+    this.isLoan =
+      this.creditType === "c" ||
+      this.creditType === "i" ||
+      this.creditType === "m";
 
-    console.log(this.maxCreditAmount)
+    this.isOpen = this.openClosed?.toLowerCase() === 'o'
 
-    this.utilPercentage = `${Math.floor(( +this.currentBalance! / +this.maxCreditAmount!) * 100)}%`
+    if (this.isLoan) {
+      this.utilPercentage = `${Math.floor(
+        (+this.currentBalance! / +this.highestBalance!) * 100
+      )}%`;
+    } else {
+      this.utilPercentage = `${Math.floor(
+        (+this.currentBalance! / +this.maxCreditAmount!) * 100
+      )}%`;
+    }
   }
-
 }
