d149a3180d5cf5837a76e7d2b8c4964aaa463a1c
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-26 13:31:39 -0800

    adding tradeline data to component

--- src/app/views/dashboard/snapshots/credit-mix/credit-mix-service/credit-mix-service.service.ts
+++ src/app/views/dashboard/snapshots/credit-mix/credit-mix-service/credit-mix-service.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { ITradeLinePartition } from "@shared/interfaces";
+import { DashboardService } from "@shared/services/dashboard/dashboard.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class CreditMixServiceService {
+export class CreditMixService {
+  tradeLinePartition: ITradeLinePartition | ITradeLinePartition[] | undefined;
 
-  constructor() { }
+  constructor(private dashboard: DashboardService) {
+    this.dashboard.tuReport$.subscribe((res) => {
+      this.tradeLinePartition = res.TrueLinkCreditReportType.TradeLinePartition;
+    });
+  }
+
+  getCreditReport(): ITradeLinePartition[] | undefined {
+    if (!this.tradeLinePartition) return
+
+    return this.tradeLinePartition instanceof Array
+      ? this.tradeLinePartition
+      : [this.tradeLinePartition];
+  }
 }
--- src/app/views/dashboard/snapshots/credit-mix/credit-mix/credit-mix.view.ts
+++ src/app/views/dashboard/snapshots/credit-mix/credit-mix/credit-mix.view.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ITradeLinePartition } from '@shared/interfaces';
+import { CreditMixService } from '../credit-mix-service/credit-mix-service.service';
 
 @Component({
   selector: 'brave-credit-mix',
-  templateUrl: './credit-mix.view.html',
-  styleUrls: ['./credit-mix.view.css']
+  templateUrl: './credit-mix.view.html'
 })
 export class CreditMixView implements OnInit {
+  tradeLineParition: ITradeLinePartition[] | undefined
 
-  constructor() { }
+  constructor(
+    private creditMix: CreditMixService
+  ) { }
 
   ngOnInit(): void {
+    this.tradeLineParition = this.creditMix.getCreditReport()
+
+    console.log('CREDIT REPORT ====>>>>>>', this.tradeLineParition)
   }
 
 }
