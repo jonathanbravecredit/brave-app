16fe20413d04b697a04f7820be221400b7286625
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-26 10:01:29 -0800

    Work on BL

--- src/app/views/dashboard/snapshots/credit-mix/components/credit-mix-badges/credit-mix-badges.component.ts
+++ src/app/views/dashboard/snapshots/credit-mix/components/credit-mix-badges/credit-mix-badges.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'brave-credit-mix-badges',
   templateUrl: './credit-mix-badges.component.html'
 })
 export class CreditMixBadgesComponent implements OnInit {
+  @Input() selectedBadge: string = 'good'
 
   constructor() { }
 
--- src/app/views/dashboard/snapshots/credit-mix/components/credit-mix-icons/credit-mix-icons.component.ts
+++ src/app/views/dashboard/snapshots/credit-mix/components/credit-mix-icons/credit-mix-icons.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'brave-credit-mix-icons',
   templateUrl: './credit-mix-icons.component.html'
 })
 export class CreditMixIconsComponent implements OnInit {
+  @Input() hasCreditCards: boolean = false
+  @Input() hasStudentLoans: boolean = true
+  @Input() hasAutoLoans: boolean = false
+  @Input() hasMortgages: boolean = true
 
   constructor() { }
 
