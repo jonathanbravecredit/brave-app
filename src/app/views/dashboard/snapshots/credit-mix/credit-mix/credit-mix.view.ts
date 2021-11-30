7e9cd50ed856823122710956ad4456381a777d89
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-11-30 15:37:26 -0800

    Added credit Mix resolver and fixed reloading issue

--- src/app/views/dashboard/snapshots/credit-mix/credit-mix/credit-mix.view.ts
+++ src/app/views/dashboard/snapshots/credit-mix/credit-mix/credit-mix.view.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { ITradeLinePartition } from "@shared/interfaces";
+import { CreditMixResolver } from "@shared/resolvers/credit-mix/credit-mix.resolver";
 import { CreditMixService } from "../credit-mix-service/credit-mix-service.service";
 import {
   ICreditMixTLSummary,
@@ -11,16 +13,24 @@ import {
   selector: "brave-credit-mix",
   templateUrl: "./credit-mix.view.html",
 })
-export class CreditMixView implements OnInit {
+export class CreditMixView {
   tradeLineParition: ITradeLinePartition[] | undefined;
   tradeLineSummary: ICreditMixTLSummary | undefined;
   recommendations: IRecommendationText | undefined;
 
-  constructor(private creditMixService: CreditMixService) {}
-
-  ngOnInit(): void {
-    this.tradeLineParition = this.creditMixService.getTradelines();
-    this.tradeLineSummary = this.creditMixService.getTradelineSummary();
-    this.recommendations = this.creditMixService.getRecommendations(this.tradeLineSummary);
+  constructor(
+    private creditMixService: CreditMixService,
+    private route: ActivatedRoute
+  ) {
+    this.route.data.subscribe((resp: any) => {
+      this.tradeLineParition = resp.tradeLineParition;
+      this.tradeLineSummary = this.creditMixService.getTradelineSummary(
+        this.tradeLineParition
+      );
+      this.recommendations = this.creditMixService.getRecommendations(
+        this.tradeLineSummary
+      );
+    });
   }
+
 }
