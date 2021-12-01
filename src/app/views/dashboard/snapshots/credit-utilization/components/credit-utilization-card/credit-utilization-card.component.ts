f101e86e1fd47d85d392a64ae3bed9c7abf97337
Jonathan <jonathan@brave.credit>
2021-11-30 17:39:01 -0800

    save

--- src/app/views/dashboard/snapshots/credit-utilization/components/credit-utilization-card/credit-utilization-card.component.ts
+++ src/app/views/dashboard/snapshots/credit-utilization/components/credit-utilization-card/credit-utilization-card.component.ts
@@ -1,33 +1,26 @@
-import {
-  AfterViewInit,
-  Component,
-  ViewChild,
-  Input,
-  OnInit,
-} from "@angular/core";
-import { ViewdetailButtonComponent } from "@shared/components/buttons/viewdetail-button/viewdetail-button.component";
-import { TransunionUtil } from "@shared/utils/transunion/transunion";
-import { TradelineDetailsTableComponent } from "@views/dashboard/reports/credit-report/tradelines/components/tradeline-details-table/tradeline-details-table.component";
-import { TradelinePaymentHistoryComponent } from "@views/dashboard/reports/credit-report/tradelines/components/tradeline-payment-history/tradeline-payment-history.component";
-import { TradelineRemarksComponent } from "@views/dashboard/reports/credit-report/tradelines/components/tradeline-remarks/tradeline-remarks.component";
-import { Observable, of } from "rxjs";
-import { ICreditUtilization, TCreditUtilizationStatus } from "./interfaces";
+import { AfterViewInit, Component, ViewChild, Input, OnInit } from '@angular/core';
+import { ViewdetailButtonComponent } from '@shared/components/buttons/viewdetail-button/viewdetail-button.component';
+import { TransunionUtil } from '@shared/utils/transunion/transunion';
+import { TradelineDetailsTableComponent } from '@views/dashboard/reports/credit-report/tradelines/components/tradeline-details-table/tradeline-details-table.component';
+import { TradelinePaymentHistoryComponent } from '@views/dashboard/reports/credit-report/tradelines/components/tradeline-payment-history/tradeline-payment-history.component';
+import { TradelineRemarksComponent } from '@views/dashboard/reports/credit-report/tradelines/components/tradeline-remarks/tradeline-remarks.component';
+import { Observable, of } from 'rxjs';
+import { ICreditUtilization, TCreditUtilizationStatus } from './interfaces';
 
 @Component({
-  selector: "brave-credit-utilization-card",
-  templateUrl: "./credit-utilization-card.component.html",
+  selector: 'brave-credit-utilization-card',
+  templateUrl: './credit-utilization-card.component.html',
 })
 export class CreditUtilizationCardComponent implements AfterViewInit, OnInit {
   @ViewChild(ViewdetailButtonComponent)
   viewDetail: ViewdetailButtonComponent | undefined;
   open$: Observable<boolean> = of(false);
 
-  @Input() status: TCreditUtilizationStatus = "good";
+  @Input() status: TCreditUtilizationStatus = 'good';
 
   @Input() creditUtilization: ICreditUtilization | undefined;
 
-  @Input() creditUtilizationType: "credit" | "credit-utilization" | "loan" =
-    "credit";
+  @Input() creditUtilizationType: 'credit' | 'credit-utilization' | 'loan' = 'credit';
 
   @Input() open: boolean = false;
 
@@ -47,9 +40,8 @@ export class CreditUtilizationCardComponent implements AfterViewInit, OnInit {
   ngOnInit(): void {
     this.percetangeUtilization = this.calculatePercentageUtilization(
       this.creditUtilization!.currentBalance,
-      this.creditUtilization!.creditLimit
+      this.creditUtilization!.creditLimit,
     );
-
     this.creditStatus = this.calculateCreditStatus(this.percetangeUtilization);
   }
 
@@ -61,7 +53,7 @@ export class CreditUtilizationCardComponent implements AfterViewInit, OnInit {
 
   calculatePercentageUtilization(
     currentBalence: string | number | undefined,
-    creditLimit: string | number | undefined
+    creditLimit: string | number | undefined,
   ): number | string | undefined {
     if (currentBalence === undefined || creditLimit === undefined) {
       return undefined;
@@ -75,37 +67,39 @@ export class CreditUtilizationCardComponent implements AfterViewInit, OnInit {
       let perc = (+currentBalence / +creditLimit) * 100;
 
       if (perc > 0 && perc < 1) {
-        return '<1'
+        return '<1';
       } else {
-        return Math.floor(perc)
+        return Math.floor(perc);
       }
     }
 
     return undefined;
   }
 
-  calculateCreditStatus(
-    percetangeUtilization: number | string | undefined
-  ): string {
+  calculateCreditStatus(percetangeUtilization: number | string | undefined): string {
     if (!this.open) {
-      return "closed";
+      return 'closed';
+    }
+
+    if (percetangeUtilization === undefined) {
+      return 'closed';
     }
 
     if (percetangeUtilization === '<1') {
-      return "excellent"
+      return 'excellent';
     }
 
     switch (true) {
       case percetangeUtilization! <= 9:
-        return "excellent";
+        return 'excellent';
       case percetangeUtilization! <= 29:
-        return "good";
+        return 'good';
       case percetangeUtilization! <= 49:
-        return "fair";
+        return 'fair';
       case percetangeUtilization! <= 74:
-        return "poor";
+        return 'poor';
       default:
-        return "verypoor";
+        return 'verypoor';
     }
   }
 }
