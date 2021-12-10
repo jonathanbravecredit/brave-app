8e81a90847bf7302b707e48417e37619787eac6b
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-12-10 14:02:14 -0800

    work on facebook link

--- src/app/shared/components/share-buttons/facebook-share/facebook-share.component.ts
+++ src/app/shared/components/share-buttons/facebook-share/facebook-share.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'brave-facebook-share',
-  templateUrl: './facebook-share.component.html'
+  selector: "brave-facebook-share",
+  templateUrl: "./facebook-share.component.html",
 })
 export class FacebookShareComponent implements OnInit {
+  @Input() userReferralLink: string | undefined;
+  fullReferralLink: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.fullReferralLink =
+      "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fapp.brave.credit%2Fauth%2Fsignup%3FreferralCode%3D" +
+      this.userReferralLink +
+      "&amp;src=sdkpreparse";
   }
-
 }
