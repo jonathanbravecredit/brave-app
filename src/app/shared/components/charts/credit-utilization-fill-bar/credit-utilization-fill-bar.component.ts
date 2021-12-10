ca7303dbe7389a5a5be073b7acc2786740f14f2e
Jonathan <jonathan@brave.credit>
2021-12-10 08:58:54 -0800

    credit utilization bar fix

--- src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
+++ src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: "brave-credit-utilization-fill-bar",
-  templateUrl: "./credit-utilization-fill-bar.component.html",
+  selector: 'brave-credit-utilization-fill-bar',
+  templateUrl: './credit-utilization-fill-bar.component.html',
 })
 export class CreditUtilizationFillBarComponent implements OnInit {
   @Input() creditType: string | undefined;
@@ -10,6 +10,7 @@ export class CreditUtilizationFillBarComponent implements OnInit {
   @Input() currentBalance: string | number | undefined;
   @Input() highestBalance: string | number | undefined;
   @Input() openClosed: string | undefined;
+  @Input() creditLimit: string | number | undefined;
   utilPercentage: number | undefined;
   isLoan: boolean = false;
   isOpen: boolean = false;
@@ -17,18 +18,19 @@ export class CreditUtilizationFillBarComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.isLoan =
-      this.creditType === "c" ||
-      this.creditType === "i" ||
-      this.creditType === "m";
+    this.isLoan = this.creditType === 'c' || this.creditType === 'i' || this.creditType === 'm';
 
-    this.isOpen = this.openClosed?.toLowerCase() === "o";
+    this.isOpen = this.openClosed?.toLowerCase() === 'o';
 
     if (this.isLoan) {
-      this.utilPercentage = ((+this.highestBalance! - +this.currentBalance!) /
-          +this.highestBalance!);
+      this.utilPercentage = (+this.highestBalance! - +this.currentBalance!) / +this.highestBalance!;
     } else {
-      this.utilPercentage = (+this.currentBalance! / +this.maxCreditAmount!);
+      const creditLimit = +(this.creditLimit || 0);
+      if (!creditLimit || `${creditLimit}` === 'NaN') {
+        this.utilPercentage = 0;
+      } else {
+        this.utilPercentage = +this.currentBalance! / +this.maxCreditAmount!;
+      }
     }
   }
 
