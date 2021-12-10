d463220301a6d1cab097fb25c889ff8e6da2fb7a
Jonathan <jonathan@brave.credit>
2021-12-09 22:22:10 -0800

    hot fix utilization

--- src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
+++ src/app/shared/components/charts/credit-utilization-fill-bar/credit-utilization-fill-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: "brave-credit-utilization-fill-bar",
-  templateUrl: "./credit-utilization-fill-bar.component.html",
+  selector: 'brave-credit-utilization-fill-bar',
+  templateUrl: './credit-utilization-fill-bar.component.html',
 })
 export class CreditUtilizationFillBarComponent implements OnInit {
   @Input() creditType: string | undefined;
@@ -10,30 +10,29 @@ export class CreditUtilizationFillBarComponent implements OnInit {
   @Input() currentBalance: string | number | undefined;
   @Input() highestBalance: string | number | undefined;
   @Input() openClosed: string | undefined;
-  utilPercentage: string | undefined;
+  utilPercentage: number | undefined; //string | undefined;
   isLoan: boolean = false;
   isOpen: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.isLoan =
-      this.creditType === "c" ||
-      this.creditType === "i" ||
-      this.creditType === "m";
+    this.isLoan = this.creditType === 'c' || this.creditType === 'i' || this.creditType === 'm';
 
-    this.isOpen = this.openClosed?.toLowerCase() === "o";
+    this.isOpen = this.openClosed?.toLowerCase() === 'o';
 
     if (this.isLoan) {
-      this.utilPercentage = `${Math.floor(
-        ((+this.highestBalance! - +this.currentBalance!) /
-          +this.highestBalance!) *
-          100
-      )}%`;
+      this.utilPercentage = Math.floor((+this.highestBalance! - +this.currentBalance!) / +this.highestBalance!);
     } else {
-      this.utilPercentage = `${Math.floor(
-        (+this.currentBalance! / +this.maxCreditAmount!) * 100
-      )}%`;
+      this.utilPercentage = Math.floor((+this.currentBalance! / +this.maxCreditAmount!) * 100);
+    }
+  }
+
+  get barWidth(): number {
+    if (this.isLoan) {
+      return 1 - (this.utilPercentage || 0);
+    } else {
+      return this.utilPercentage || 0;
     }
   }
 }
--- src/app/shared/resolvers/referral/referral.resolver.ts
+++ src/app/shared/resolvers/referral/referral.resolver.ts
@@ -1,19 +1,10 @@
-import { Injectable } from "@angular/core";
-import {
-  Router,
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
-} from "@angular/router";
-import {
-  IGroupedYearMonthReferral,
-  IPayments,
-  IReferral,
-} from "@shared/interfaces/referrals.interface";
-import { ReferralMetricsResolver } from "@shared/resolvers/referral-metrics/referral-metrics.resolver";
-import { ReferralRecordResolver } from "@shared/resolvers/referral-record/referral-record.resolver";
-import { InterstitialService } from "@shared/services/interstitial/interstitial.service";
-import { ReferralPaymentsResolver } from "../referral-payments/referral-payments.resolver";
+import { Injectable } from '@angular/core';
+import { Router, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { IGroupedYearMonthReferral, IPayments, IReferral } from '@shared/interfaces/referrals.interface';
+import { ReferralMetricsResolver } from '@shared/resolvers/referral-metrics/referral-metrics.resolver';
+import { ReferralRecordResolver } from '@shared/resolvers/referral-record/referral-record.resolver';
+import { InterstitialService } from '@shared/services/interstitial/interstitial.service';
+import { ReferralPaymentsResolver } from '../referral-payments/referral-payments.resolver';
 
 export interface IReferralResolver {
   metrics: IGroupedYearMonthReferral[] | null;
@@ -22,28 +13,38 @@ export interface IReferralResolver {
 }
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ReferralResolver implements Resolve<IReferralResolver> {
   constructor(
     private interstitial: InterstitialService,
     protected referralMetrics: ReferralMetricsResolver,
     protected referralRecord: ReferralRecordResolver,
-    protected referralPayments: ReferralPaymentsResolver
+    protected referralPayments: ReferralPaymentsResolver,
   ) {}
-  async resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Promise<IReferralResolver> {
-    this.interstitial.changeMessage(" ");
+  async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<IReferralResolver> {
+    this.interstitial.changeMessage(' ');
     this.interstitial.openInterstitial();
-    const metrics = await this.referralMetrics.resolve(route, state);
-    const referral = await this.referralRecord.resolve(route, state);
+
+    let metrics;
+    try {
+      metrics = await this.referralMetrics.resolve(route, state);
+    } catch {
+      metrics = null;
+    }
+
+    let referral;
+    try {
+      referral = await this.referralRecord.resolve(route, state);
+    } catch {
+      referral = null;
+    }
+
     let payments;
     try {
       payments = await this.referralPayments.resolve(route, state);
     } catch {
-      payments = null
+      payments = null;
     }
 
     return {
