4261af882b61b1f1a7640c4922212af458e32ff6
Jonathan <jonathan@brave.credit>
2021-12-09 21:36:23 -0800

    adding analytics profile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   api: 'https://0h0d8u4rf7.execute-api.us-east-2.amazonaws.com/prod',
   marketing: 'https://7dvutnx2o5.execute-api.us-east-2.amazonaws.com/prod',
+  analytics: 'https://2piy9ocdre.execute-api.us-east-2.amazonaws.com/prod',
 };
