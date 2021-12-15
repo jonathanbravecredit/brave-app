7b1425bb3121cfad508fa66c7228e85979881c17
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-12-15 11:16:06 -0800

    fixes to the fb twitter icons

--- src/app/shared/components/share-buttons/twitter-share/twitter-share.component.ts
+++ src/app/shared/components/share-buttons/twitter-share/twitter-share.component.ts
@@ -12,7 +12,7 @@ export class TwitterShareComponent implements OnInit {
 
   ngOnInit(): void {
     this.fullReferralLink =
-      "https://twitter.com/intent/tweet?text=Haven’t%20checked%20your%20credit%20score%20in%20a%20while?%20Check%20it%20through%20Brave%20Credit%20and%20get%20$5!%20Plus,%20get%20$5%20for%20anyone%20else%20you%20refer!%20Then,%20dispute%20any%20issues%20you%20see%20for%20free,%20and%20start%20growing%20your%20score!%20https%3A%2F%2Fapp.brave.credit%2Fauth%2Fsignup%3FreferralCode%3D" +
-      this.referralCode
+      "https://twitter.com/intent/tweet?text=Haven’t%20checked%20your%20credit%20score%20in%20a%20while?%20Check%20it%20through%20Brave%20Credit%20and%20get%20$5!%20Plus,%20get%20$5%20for%20anyone%20else%20you%20refer!%20Then,%20dispute%20any%20issues%20you%20see%20for%20free,%20and%20start%20growing%20your%20score!%20%23BraveCredit%20https%3A%2F%2Fapp.brave.credit%2Fauth%2Fsignup%3FreferralCode%3D" +
+      this.referralCode;
   }
 }
