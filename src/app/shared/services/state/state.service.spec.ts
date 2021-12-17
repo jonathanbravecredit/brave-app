93d8490cd09e98dfba1a13755df633e521e84e03
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-12-16 17:00:31 -0800

    services

--- src/app/shared/services/state/state.service.spec.ts
+++ src/app/shared/services/state/state.service.spec.ts
@@ -1,3 +1,4 @@
+import { state } from "@angular/animations";
 import { TestBed } from "@angular/core/testing";
 import { Store } from "@ngxs/store";
 import { of } from "rxjs";
@@ -8,13 +9,15 @@ import { StateService } from "./state.service";
 describe("StateService", () => {
   let service: StateService;
   let apiMock: any;
+  let storeMock: any;
 
   beforeEach(() => {
     apiMock = jasmine.createSpyObj("ApiService", ["UpdateAppData"]);
+    storeMock = of();
     TestBed.configureTestingModule({
       providers: [
         { provide: APIService, useValue: apiMock },
-        Store
+        { provide: Store, useValue: storeMock },
       ],
     });
     service = TestBed.inject(StateService);
--- src/app/shared/services/sync/sync.service.spec.ts
+++ src/app/shared/services/sync/sync.service.spec.ts
@@ -1,16 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { Store } from "@ngxs/store";
+import { APIService } from "../aws/api.service";
+import { StateService } from "../state/state.service";
 
-import { SyncService } from './sync.service';
+import { SyncService } from "./sync.service";
 
-describe('SyncService', () => {
+//private api: APIService, private store: Store, private router: Router, private statesvc: StateService
+
+describe("SyncService", () => {
   let service: SyncService;
+  let apiMock: any;
+  let storeMock: any;
+  let routerMock: any;
+  let statesvcMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    apiMock = jasmine.createSpyObj("APIService", [""]);
+
+    storeMock = jasmine.createSpyObj("Store", [""]);
+
+    routerMock = jasmine.createSpyObj("Router", [""]);
+
+    statesvcMock = jasmine.createSpyObj("StateService", [""]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: APIService, useValue: apiMock },
+        { provide: Store, useValue: storeMock },
+        { provide: Router, useValue: routerMock },
+        { provide: StateService, useValue: statesvcMock },
+      ],
+    });
     service = TestBed.inject(SyncService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
