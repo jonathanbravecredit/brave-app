e6851be66e21290019d609ed7b46fc89a6f13e19
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-12-20 15:31:19 -0800

    added suspended referral template

--- src/app/views/dashboard/snapshots/referral-dashboard/referral-dashboard-pure/referral-pure.view.ts
+++ src/app/views/dashboard/snapshots/referral-dashboard/referral-dashboard-pure/referral-pure.view.ts
@@ -1,16 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IGroupedYearMonthReferral, IPayments, IReferral } from '@shared/interfaces/referrals.interface';
+import { Component, Input, OnInit } from "@angular/core";
+import {
+  IGroupedYearMonthReferral,
+  IPayments,
+  IReferral,
+} from "@shared/interfaces/referrals.interface";
 
 @Component({
-  selector: 'brave-referral-pure',
-  templateUrl: './referral-pure.view.html',
+  selector: "brave-referral-pure",
+  templateUrl: "./referral-pure.view.html",
 })
 export class ReferralDashboardPureView implements OnInit {
   @Input() referral: IReferral | undefined;
   @Input() metrics: IGroupedYearMonthReferral[] = [];
   @Input() payments: IPayments | undefined;
+  private _isSuspended: boolean = false;
+  set isSuspended(value: boolean) {
+    this._isSuspended = value;
+  }
+  get isSuspended(): boolean {
+    return this._isSuspended
+  }
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.referral?.status === "suspended") {
+      this.isSuspended = true;
+    };
+  }
 }
