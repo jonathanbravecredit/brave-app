f3b9acafd609dd6f406641bb83b453256d62d720
Jonathan <jonathan@brave.credit>
2021-12-27 11:11:13 -0800

    WIP creditreport specs

--- src/app/shared/services/creditreport/creditreport.service.spec.ts
+++ src/app/shared/services/creditreport/creditreport.service.spec.ts
@@ -1,25 +1,23 @@
-import { TestBed } from "@angular/core/testing";
-import { StateService } from "../state/state.service";
-import { TransunionService } from "../transunion/transunion.service";
-
-import { CreditreportService } from "./creditreport.service";
+import { TestBed, tick } from '@angular/core/testing';
+import { StateService } from '../state/state.service';
+import { TransunionService } from '../transunion/transunion.service';
+import { Helper } from '@testing/index';
+import { CreditreportService } from './creditreport.service';
+import { AgenciesStateModel } from '@store/agencies/agencies.model';
+import { of, Subscription } from 'rxjs';
+import { TransunionInput } from '@shared/services/aws/api.service';
 
 //private statesvc: StateService, private transunion: TransunionService
 
-describe("CreditreportService", () => {
+describe('CreditreportService', () => {
   let service: CreditreportService;
   let stateMock: any;
   let transunionMock: any;
+  let h: Helper<CreditreportService>;
 
   beforeEach(() => {
-    stateMock = jasmine.createSpyObj(
-      "StateService",
-      ["updateAgenciesAsync", "updateAgencies"],
-      ["state"]
-    );
-    transunionMock = jasmine.createSpyObj("TransunionService", [
-      "refreshCreditReport",
-    ]);
+    stateMock = jasmine.createSpyObj('StateService', ['updateAgenciesAsync', 'updateAgencies'], ['state']);
+    transunionMock = jasmine.createSpyObj('TransunionService', ['refreshCreditReport']);
     TestBed.configureTestingModule({
       providers: [
         { provide: StateService, useValue: stateMock },
@@ -27,9 +25,56 @@ describe("CreditreportService", () => {
       ],
     });
     service = TestBed.inject(CreditreportService);
+    h = new Helper(service);
   });
 
-  it("should be created", () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Service constructor', () => {
+    it('Should assign agenciesSub$ on constructor', () => {
+      const test = service.agenciesSub$ instanceof Subscription;
+      expect(test).toBeTrue();
+    });
+    it('Should NOT set the tuAgency property when the agency tu state is empty', () => {
+      const tuMock = {} as TransunionInput;
+      const agencyMock = {
+        transunion: tuMock,
+      };
+      service.agencies$ = of(agencyMock);
+      tick();
+      const test = service.tuAgency === undefined;
+      expect(test).toBeTrue();
+    });
+    it('Should set the tuAgency property when the agency tu state is NOT empty', (done) => {
+      const tuMock = { authenticated: true } as TransunionInput;
+      const agencyMock = {
+        transunion: tuMock,
+      };
+      service.agencies$ = of(agencyMock);
+      tick();
+      const test = service.tuAgency !== undefined;
+      expect(test).toBeTrue();
+    });
+  });
+
+  describe('Method calls', () => {
+    it('Should call transunion refreshCreditReport when refreshCreditReport service method is called', () => {
+      service.refreshCreditReport();
+      expect(transunionMock.refreshCreditReport).toHaveBeenCalled();
+    });
+
+    it('Get Transunion should return return a blank object when transunion data is not populated on oagencies', () => {
+      const tu = { agencies: { transunion: null } } as AgenciesStateModel;
+      const test = service.getTransunion(tu);
+      expect(test).toEqual({});
+    });
+
+    it('Get Transunion should return return a blank object when transunion data is not populated on oagencies', () => {
+      const tu = { agencies: { transunion: { authenticated: true } } } as AgenciesStateModel;
+      const test = service.getTransunion(tu);
+      expect(test.authenticated).toEqual(true);
+    });
   });
 });
