04ba6822c7cdab7705b5a1953fdc26f063c46482
NCHitsman <noah.hitsman@gmail.com>
2022-01-05 14:31:31 -0800

    work on referrals service fixes

--- src/app/shared/services/referrals/referrals.service.ts
+++ src/app/shared/services/referrals/referrals.service.ts
@@ -15,6 +15,7 @@ export class ReferralsService implements OnDestroy {
   campaign = CURRENT_CAMPAIGN;
   referredByCode$ = new BehaviorSubject<string | null>(null);
   isActive: boolean = false;
+  isActive$ = new BehaviorSubject<boolean>(false);
   isActiveSub$: Subscription | undefined;
 
   constructor(
@@ -25,6 +26,7 @@ export class ReferralsService implements OnDestroy {
   ) {
     this.isActiveSub$ = this.feature.referrals$.subscribe((isActive) => {
       this.isActive = isActive;
+      this.isActive$.next(isActive);
     });
   }
 
@@ -92,7 +94,12 @@ export class ReferralsService implements OnDestroy {
    * @returns
    */
   async getReferralMonthlyCampaignEarnings(month?: string, year?: string): Promise<IGroupedYearMonthReferral[]> {
+    console.log('ACTIVE', this.isActive);
+
     if (!this.isActive) return [];
+
+    console.log('HERE');
+
     const url = `${environment.marketing}/referral/campaign/earnings/monthly`;
     const token = await this.auth.getIdTokenJwtTokens();
     const headers = new HttpHeaders({
--- src/app/views/dashboard/snapshots/referral-dashboard/referral-dashboard/referral-dashboard.view.ts
+++ src/app/views/dashboard/snapshots/referral-dashboard/referral-dashboard/referral-dashboard.view.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IGroupedYearMonthReferral, IPayments, IReferral } from '@shared/interfaces/referrals.interface';
+import { ReferralsService } from '@shared/services/referrals/referrals.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'brave-referral-dashboard',
   templateUrl: './referral-dashboard.view.html',
 })
-export class ReferralDashboardView implements OnInit {
+export class ReferralDashboardView implements OnInit, OnDestroy {
   metrics: IGroupedYearMonthReferral[] = [];
   referral: IReferral | undefined;
   payments: IPayments | undefined;
   disabled: boolean = false;
-  constructor(private route: ActivatedRoute) {
+  isActiveSub$: Subscription | undefined;
+
+  constructor(private route: ActivatedRoute, private referralService: ReferralsService) {
     this.route.data.subscribe((resp: any) => {
       this.metrics = resp.referral.metrics;
       this.referral = resp.referral.referral;
       this.payments = resp.referral.payments;
     });
+
+    this.isActiveSub$ = this.referralService.isActive$.subscribe((isActive) => {
+      this.disabled = isActive;
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('TEST ==>>', this.payments);
+
+    console.log('METRICS', this.metrics);
+  }
+
+  ngOnDestroy(): void {
+    this.isActiveSub$?.unsubscribe();
+  }
 }
