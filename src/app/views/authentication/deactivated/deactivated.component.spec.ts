870e4ffd1600a48f0a1d1e595704557124345b31
NCHitsman <noah.hitsman@gmail.com>
2022-01-06 15:03:46 -0800

    added small deactivated test

--- src/app/views/authentication/deactivated/deactivated.component.spec.ts
+++ src/app/views/authentication/deactivated/deactivated.component.spec.ts
@@ -1,14 +1,15 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { Router } from "@angular/router";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { deactivatedContent } from '@views/authentication/deactivated/content';
 
-import { DeactivatedComponent } from "./deactivated.component";
+import { DeactivatedComponent } from './deactivated.component';
 
-describe("DeactivatedComponent", () => {
+describe('DeactivatedComponent', () => {
   let component: DeactivatedComponent;
   let fixture: ComponentFixture<DeactivatedComponent>;
   let routerMock: any;
   beforeEach(async () => {
-    routerMock = jasmine.createSpyObj("", [""]);
+    routerMock = jasmine.createSpyObj('', ['']);
     await TestBed.configureTestingModule({
       declarations: [DeactivatedComponent],
       providers: [{ provide: Router, useValue: routerMock }],
@@ -21,7 +22,11 @@ describe("DeactivatedComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the proper content', () => {
+    expect(component.content).toEqual(deactivatedContent);
   });
 });
