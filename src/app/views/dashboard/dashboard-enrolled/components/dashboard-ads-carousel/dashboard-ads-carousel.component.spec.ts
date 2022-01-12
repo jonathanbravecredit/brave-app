608d4020690eb65f33b6be9dc84763cf6eb30a77
NCHitsman <noah.hitsman@gmail.com>
2022-01-12 10:05:26 -0800

    Added test

--- src/app/views/dashboard/dashboard-enrolled/components/dashboard-ads-carousel/dashboard-ads-carousel.component.spec.ts
+++ src/app/views/dashboard/dashboard-enrolled/components/dashboard-ads-carousel/dashboard-ads-carousel.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IAdData } from '@shared/interfaces/ads.interface';
 
 import { DashboardAdsCarouselComponent } from './dashboard-ads-carousel.component';
 
@@ -22,4 +23,12 @@ describe('DashboardAdsCarouselComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create the same amount of pages as ad objects are passed in', () => {
+    component.adsData = [{} as IAdData, {} as IAdData, {} as IAdData, {} as IAdData]
+
+    component.ngOnInit()
+
+    expect(component.pages.length).toEqual(4)
+  })
 });
