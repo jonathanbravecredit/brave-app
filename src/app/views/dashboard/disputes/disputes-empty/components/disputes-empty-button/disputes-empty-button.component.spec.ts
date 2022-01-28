81975c19e49cd381dbe4c3a33b10a699867a03de
NCHitsman <noah.hitsman@gmail.com>
2022-01-28 09:17:39 -0800

    work on disputes tests

--- src/app/views/dashboard/disputes/disputes-empty/components/disputes-empty-button/disputes-empty-button.component.spec.ts
+++ src/app/views/dashboard/disputes/disputes-empty/components/disputes-empty-button/disputes-empty-button.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { DisputesEmptyButtonComponent } from './disputes-empty-button.component';
 
 describe('DisputesEmptyButtonComponent', () => {
   let component: DisputesEmptyButtonComponent;
   let fixture: ComponentFixture<DisputesEmptyButtonComponent>;
+  let routerMock: any;
 
   beforeEach(async () => {
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
-      declarations: [ DisputesEmptyButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [DisputesEmptyButtonComponent],
+      providers: [{ provide: Router, useValue: routerMock }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +25,11 @@ describe('DisputesEmptyButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run router navigate when buttonClick', () => {
+    component.buttonClick();
+
+    expect(routerMock.navigate).toHaveBeenCalled();
   });
 });
--- src/app/views/dashboard/disputes/disputes-findings/dispute-findings/dispute-findings.view.spec.ts
+++ src/app/views/dashboard/disputes/disputes-findings/dispute-findings/dispute-findings.view.spec.ts
@@ -2,34 +2,36 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { IDispute } from '@shared/interfaces/disputes';
 import { DisputeService } from '@shared/services/dispute/dispute.service';
-import { BehaviorSubject, of } from 'rxjs';
+import { BehaviorSubject, of, Subscription } from 'rxjs';
 
 import { DisputeFindingsView } from './dispute-findings.view';
 
 describe('DisputeFindingsView', () => {
   let component: DisputeFindingsView;
   let fixture: ComponentFixture<DisputeFindingsView>;
-  class RouteMock {
-    data = of({
-      reports: {
-        investigationResults: {
-          record: null,
-        },
-        creditBureauResults: {
-          record: null,
-        },
-      },
-    });
-  }
+  let routeMock: any;
   let disputeServiceMock: any;
 
   beforeEach(async () => {
     disputeServiceMock = jasmine.createSpyObj('DisputeService', ['getUserStateOfResidence']);
     disputeServiceMock.currentDispute$ = new BehaviorSubject<IDispute>({} as IDispute);
+    routeMock = jasmine.createSpyObj('ActivatedRoute', [''], {
+      data: of({
+        reports: {
+          investigationResults: {
+            record: null,
+          },
+          creditBureauResults: {
+            record: null,
+          },
+        },
+      }),
+    });
+
     await TestBed.configureTestingModule({
       declarations: [DisputeFindingsView],
       providers: [
-        { provide: ActivatedRoute, useClass: RouteMock },
+        { provide: ActivatedRoute, useValue: routeMock },
         { provide: DisputeService, useValue: disputeServiceMock },
       ],
     }).compileComponents();
@@ -43,5 +45,19 @@ describe('DisputeFindingsView', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run disputeService.getUserStateOfResidence on init', () => {
+    component.ngOnInit();
+
+    expect(disputeServiceMock.getUserStateOfResidence).toHaveBeenCalled();
+  });
+
+  it('should unsubscribe from routeSub$ ondestroy', () => {
+    spyOn(component.routeSub$!, 'unsubscribe')
+
+    component.ngOnDestroy();
+
+    expect(component.routeSub$?.unsubscribe).toHaveBeenCalled();
   });
 });
