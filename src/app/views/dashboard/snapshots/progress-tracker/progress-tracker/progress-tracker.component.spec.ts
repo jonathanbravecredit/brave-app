37236462f9b53930fa965b96a806c5e8002c881c
NCHitsman <noah.hitsman@gmail.com>
2022-03-01 14:51:23 -0800

    wip

--- src/app/views/dashboard/snapshots/progress-tracker/progress-tracker/progress-tracker.component.spec.ts
+++ src/app/views/dashboard/snapshots/progress-tracker/progress-tracker/progress-tracker.component.spec.ts
@@ -1,26 +1,27 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Store } from '@ngxs/store';
+import { Initiative } from '@shared/interfaces/progress-tracker.interface';
 import { DashboardService } from '@shared/services/dashboard/dashboard.service';
+import { ProgressTrackerService } from '@shared/services/progress-tracker/progress-tracker-service.service';
 import { of } from 'rxjs';
 
 import { ProgressTrackerComponent } from './progress-tracker.component';
 
-// private dashboardService: DashboardService, private store: Store
-
 describe('ProgressTrackerComponent', () => {
   let component: ProgressTrackerComponent;
   let fixture: ComponentFixture<ProgressTrackerComponent>;
-  let dashboardMock: any;
+  let progressTrackerMock: any;
   let storeMock: any;
 
   beforeEach(async () => {
-    dashboardMock = jasmine.createSpyObj('DashboardService', [''], {progressTrackerData$: of()});
+    progressTrackerMock = jasmine.createSpyObj('ProgressTrackerService', ['findFutureScore']);
     storeMock = jasmine.createSpyObj('Store', ['selectSnapshot']);
 
     await TestBed.configureTestingModule({
       declarations: [ProgressTrackerComponent],
       providers: [
-        { provide: DashboardService, useValue: dashboardMock },
+        { provide: ProgressTrackerService, useValue: progressTrackerMock },
         { provide: Store, useValue: storeMock },
       ],
     }).compileComponents();
@@ -34,5 +35,63 @@ describe('ProgressTrackerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run setCurrentInitiativeTasks on init', () => {
+    const spy = spyOn(component, 'setCurrentInitiativeTasks');
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set futureScore to 0 if FindFutureScore and enrolledScore are falsy', () => {
+    progressTrackerMock.findFutureScore.and.returnValue(undefined);
+
+    component.enrolledScore = undefined;
+
+    component.ngOnInit();
+
+    expect(component.futureScore).toEqual(0);
+  });
+
+  it('should set futureScore to 123 if FindFutureScore returns 123 and enrolledScore is falsy', () => {
+    progressTrackerMock.findFutureScore.and.returnValue(123);
+
+    component.enrolledScore = undefined;
+
+    component.ngOnInit();
+
+    expect(component.futureScore).toEqual(123);
+  });
+
+  it('should set futureScore to 50 if FindFutureScore returns 25 and enrolledScore is 25', () => {
+    progressTrackerMock.findFutureScore.and.returnValue(25);
+
+    component.enrolledScore = '25';
+
+    component.ngOnInit();
+
+    expect(component.futureScore).toEqual(50);
+  });
+
+  it('should set initiativeTasks to [] if initiative is falsy', () => {
+    component.initiative = null;
+
+    component.setCurrentInitiativeTasks();
+
+    expect(component.initiativeTasks).toEqual([]);
+  });
+
+  it('should set initiative to the initiative in the progresstackerservice', () => {
+    const testInitiative = { id: 'test' } as Initiative;
+
+    progressTrackerMock.initiative = testInitiative;
+
+    fixture = TestBed.createComponent(ProgressTrackerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.initiative).toEqual(testInitiative);
   });
 });
