1dab994bd7d71aa73481aef3c5781e431f2626fd
Jonathan <jonathan@brave.credit>
2022-02-28 18:47:07 -0800

    refactor risk score pipe

--- src/app/shared/pipes/parse-risk-score/parse-risk-score.pipe.ts
+++ src/app/shared/pipes/parse-risk-score/parse-risk-score.pipe.ts
@@ -7,12 +7,25 @@ import { IMergeReport } from '@shared/interfaces/merge-report.interface';
 export class ParseRiskScorePipe implements PipeTransform {
   transform(report: IMergeReport | undefined): number {
     if (report === undefined) return -1;
-    const creditScore = report?.TrueLinkCreditReportType?.Borrower?.CreditScore.find(
-      (ele) => ele.scoreName?.toLowerCase() === 'vantagescore3',
-    );
-    const score = creditScore?.riskScore;
+    const creditScore = report?.TrueLinkCreditReportType?.Borrower?.CreditScore;
+    const item = creditScore.find((ele) => ele.scoreName?.toLowerCase() === 'vantagescore3') || null;
+    const score = item?.riskScore;
     const _score = Math.round(score as number);
     if (isNaN(_score)) return -1;
     return _score;
+    // if (creditScore instanceof Array) {
+    // } else {
+    //   const score = creditScore?.riskScore;
+    //   const _score = Math.round(score as number);
+    //   if (isNaN(_score)) return -1;
+    //   return _score;
+    // }
+    // const creditScore = report?.TrueLinkCreditReportType?.Borrower?.CreditScore.find(
+    //   (ele) => ele.scoreName?.toLowerCase() === 'vantagescore3',
+    // );
+    // const score = creditScore?.riskScore;
+    // const _score = Math.round(score as number);
+    // if (isNaN(_score)) return -1;
+    // return _score;
   }
 }
