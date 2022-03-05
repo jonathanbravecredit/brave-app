828fa2d185274baf9d8c72fb4ee7921c34ff414e
Jonathan <jonathan@brave.credit>
2022-03-04 16:39:13 -0800

    save

--- src/app/shared/services/monitor/rendered/rendered.service.spec.ts
+++ src/app/shared/services/monitor/rendered/rendered.service.spec.ts
@@ -1,12 +1,19 @@
+import { NgZone } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { RenderedService } from './rendered.service';
 
 describe('RenderedService', () => {
   let service: RenderedService;
+  let zoneMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    zoneMock = jasmine.createSpyObj('NgZone', { onMicrotaskEmpry: of(null) });
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: NgZone, useValue: zoneMock }],
+    });
     service = TestBed.inject(RenderedService);
   });
 
