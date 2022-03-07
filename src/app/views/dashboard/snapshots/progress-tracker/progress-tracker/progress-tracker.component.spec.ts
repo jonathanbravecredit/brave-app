af44e1f75a28bcf4c0f88561c093473638ddfe79
NCHitsman <noah.hitsman@gmail.com>
2022-03-07 11:22:01 -0800

    fix

--- src/app/views/dashboard/snapshots/progress-tracker/progress-tracker/progress-tracker.component.spec.ts
+++ src/app/views/dashboard/snapshots/progress-tracker/progress-tracker/progress-tracker.component.spec.ts
@@ -1,19 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Initiative } from '@shared/interfaces/progress-tracker.interface';
+import { DashboardService } from '@shared/services/dashboard/dashboard.service';
 import { ProgressTrackerService } from '@shared/services/progress-tracker/progress-tracker-service.service';
+import { of } from 'rxjs';
 import { ProgressTrackerComponent } from './progress-tracker.component';
+
+//public progressTracker: ProgressTrackerService, public dashboard: DashboardService
 
 describe('ProgressTrackerComponent', () => {
   let component: ProgressTrackerComponent;
   let fixture: ComponentFixture<ProgressTrackerComponent>;
   let progressTrackerMock: any;
+  let dashboardMock: any;
 
   beforeEach(async () => {
     progressTrackerMock = jasmine.createSpyObj('ProgressTrackerService', ['findFutureScore']);
+    dashboardMock = jasmine.createSpyObj('DashboardService', [''], { dashScore$: of(), dashDelta$: of() });
 
     await TestBed.configureTestingModule({
       declarations: [ProgressTrackerComponent],
-      providers: [{ provide: ProgressTrackerService, useValue: progressTrackerMock }],
+      providers: [
+        { provide: ProgressTrackerService, useValue: progressTrackerMock },
+        { provide: DashboardService, useValue: dashboardMock },
+      ],
     }).compileComponents();
   });
 
