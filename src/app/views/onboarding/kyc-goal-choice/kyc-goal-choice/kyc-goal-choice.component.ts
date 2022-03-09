276d53a73a9859c403fd867bdfd2f4e1d8502d32
NCHitsman <noah.hitsman@gmail.com>
2022-03-09 13:58:25 -0800

    fixed imports

--- src/app/views/onboarding/kyc-goal-choice/kyc-goal-choice/kyc-goal-choice.component.ts
+++ src/app/views/onboarding/kyc-goal-choice/kyc-goal-choice/kyc-goal-choice.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_NAMES as routes } from '@shared/routes/routes.names';
-import { AnalyticsService } from '@shared/services/analytics/analytics/analytics.service';
-import { AnalyticPageViewEvents } from '@shared/services/analytics/analytics/constants';
 
 export interface IGoalInfo {
   programId: string;
@@ -16,7 +14,7 @@ export interface IGoalInfo {
   templateUrl: './kyc-goal-choice.component.html',
 })
 export class KycGoalChoiceComponent implements OnInit {
-  constructor(private router: Router, private analytics: AnalyticsService) {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
