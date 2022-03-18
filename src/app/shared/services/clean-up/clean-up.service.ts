fa1d509b5d97c16a1652fe00256b3a0d82922d05
NCHitsman <noah.hitsman@gmail.com>
2022-03-18 16:29:25 -0700

    adding app close

--- src/app/shared/services/clean-up/clean-up.service.ts
+++ src/app/shared/services/clean-up/clean-up.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, Renderer2 } from '@angular/core';
 import { Store } from '@ngxs/store';
 import * as AppDataActions from '@store/app-data/app-data.actions';
 import * as CreditReportActions from '@store/credit-report/credit-report.actions';
@@ -7,12 +7,29 @@ import * as ProgressTrackerActions from '@store/progress-tracker/progress-tracke
 @Injectable({
   providedIn: 'root',
 })
-export class CleanUpService {
-  constructor(private store: Store) {}
+export class CleanUpService implements OnDestroy {
+  listener: any;
+
+  constructor(private store: Store, private renderer: Renderer2) {}
 
   clearAllState(): void {
     this.store.dispatch(new AppDataActions.Delete());
     this.store.dispatch(new CreditReportActions.Delete());
     this.store.dispatch(new ProgressTrackerActions.Delete());
+  }
+
+  runOnAppClose(callback: Function): void {
+    this.listener = this.renderer.listen(window, 'beforeunload', (event: BeforeUnloadEvent) => {
+      callback();
+
+      event.returnValue = false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.listener) {
+      this.listener(); // listener returns a function to invoke which 'unlistens'
+      this.listener = null;
+    }
   }
 }
