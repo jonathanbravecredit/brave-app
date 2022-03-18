cbf5665d2575397a6a16a4d2752371f8f30de341
Jonathan <jonathan@brave.credit>
2022-03-18 11:28:33 -0700

    fixing merge issue

--- src/app/shared/services/dashboard/dashboard.service.ts
+++ src/app/shared/services/dashboard/dashboard.service.ts
@@ -106,13 +106,6 @@ export class DashboardService implements OnDestroy {
     });
   }
 
-  ngOnDestroy() {
-    this.stateSub$?.unsubscribe();
-    this.dashScoresSub$?.unsubscribe();
-    this.tuReportSub$?.unsubscribe();
-    this.updatedOnSub$?.unsubscribe();
-  }
-
   calculateDelta(scores: IProductTrendingData[] | null): number {
     if (scores && scores.length > 1) {
       let latestScore = +scores[scores.length - 1].AttributeValue;
