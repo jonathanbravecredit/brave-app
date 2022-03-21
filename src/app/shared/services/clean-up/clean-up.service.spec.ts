2ac4307b04c050c10bd437cac9a55c2b1310332c
NCHitsman <noah.hitsman@gmail.com>
2022-03-21 10:52:09 -0700

    fix

--- src/app/shared/services/clean-up/clean-up.service.spec.ts
+++ src/app/shared/services/clean-up/clean-up.service.spec.ts
@@ -1,12 +1,25 @@
+import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngxs/store';
 
 import { CleanUpService } from './clean-up.service';
 
+//private store: Store, private renderer: Renderer2
+
 describe('CleanUpService', () => {
   let service: CleanUpService;
+  let storeMock: any;
+  let renderMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    storeMock = jasmine.createSpyObj('Store', ['storeMock']);
+    renderMock = jasmine.createSpyObj('Renderer2', ['renderMock']);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Store, useValue: storeMock },
+        { provide: Renderer2, useValue: renderMock },
+      ],
+    });
     service = TestBed.inject(CleanUpService);
   });
 
