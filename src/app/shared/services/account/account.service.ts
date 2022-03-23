aeb695f30beaa93b314af236d48668f245749132
NCHitsman <noah.hitsman@gmail.com>
2022-03-23 16:51:02 -0700

    wip

--- src/app/shared/services/account/account.service.ts
+++ src/app/shared/services/account/account.service.ts
@@ -1,28 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { ITradeLinePartition } from '@shared/interfaces';
+import { IMergeReport, ISubscriber, ITradeLinePartition } from '@shared/interfaces';
 import { IDisputePersonalItem, IDisputePublicItem, IDisputeTradelineItem } from '@shared/interfaces/dispute.interfaces';
 import { DisputeService } from '@shared/services/dispute/dispute.service';
 import { InterstitialService } from '@shared/services/interstitial/interstitial.service';
 import { DisputeReconfirmFilter } from '@views/dashboard/disputes/disputes-reconfirm/types/dispute-reconfirm-filters';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { ROUTE_NAMES as routes } from '@shared/routes/routes.names';
 import { TransunionUtil as tu } from '@shared/utils/transunion/transunion';
+import { CreditreportService } from '@shared/services/creditreport/creditreport.service';
+import { MergereportToNegativeTradelinesPipe } from '@shared/pipes/mergereport-to-negative-tradelines/mergereport-to-negative-tradelines.pipe';
+import { MergereportToSubscribersPipe } from '@shared/pipes/mergereport-to-subscribers/mergereport-to-subscribers.pipe';
+import { StateService } from '@shared/services/state/state.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   accountType: 'tradeline' | 'personalItem' | 'publicItem' | undefined;
-  tradeline$: BehaviorSubject<IDisputeTradelineItem | null> = new BehaviorSubject(null);
-  personalItem$: BehaviorSubject<IDisputePersonalItem | null> = new BehaviorSubject(null);
-  publicItem$: BehaviorSubject<IDisputePublicItem | null> = new BehaviorSubject(null);
+  personalItem$: BehaviorSubject<IDisputePersonalItem | null> = new BehaviorSubject<IDisputePersonalItem | null>(null);
+  publicItem$: BehaviorSubject<IDisputePublicItem | null> = new BehaviorSubject<IDisputePublicItem | null>(null);
+  acknowledged: boolean = false;
+  creditReport$: Subscription;
+  tradelines: ITradeLinePartition[] | null = null;
+  subscribers: ISubscriber[] | null = null;
+  report: IMergeReport | undefined;
 
   constructor(
     private interstitial: InterstitialService,
     private disputeService: DisputeService,
     private router: Router,
-  ) {}
+    private creditReportService: CreditreportService,
+    private mergereportToNegativeTradelinesPipe: MergereportToNegativeTradelinesPipe,
+    private mergereportToSubscribers: MergereportToSubscribersPipe,
+    private statesvc: StateService,
+  ) {
+    this.creditReport$ = this.creditReportService.tuReport$.asObservable().subscribe((v) => {
+      this.report = v;
+    });
+    if (this.report) {
+      this.tradelines = this.mergereportToNegativeTradelinesPipe.transform(this.report);
+      this.subscribers = this.mergereportToSubscribers.transform(this.report);
+    }
+    this.acknowledged = this.statesvc.state?.appData.agencies?.transunion?.acknowledgedDisputeTerms || false;
+  }
 
   async onConfirmed(tradeline: ITradeLinePartition): Promise<void> {
     const accountType = tu.queries.report.getTradelineTypeDescription(tradeline);
--- src/app/views/dashboard/negative-account/negative-account-initial/negative-account-initial.component.ts
+++ src/app/views/dashboard/negative-account/negative-account-initial/negative-account-initial.component.ts
@@ -1,46 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IMergeReport, ITradeLinePartition } from '@shared/interfaces/merge-report.interface';
-import { CreditreportService } from '@shared/services/creditreport/creditreport.service';
-import { DisputeService } from '@shared/services/dispute/dispute.service';
+import { IMergeReport } from '@shared/interfaces/merge-report.interface';
 import { StateService } from '@shared/services/state/state.service';
-import { TransunionUtil as tu } from '@shared/utils/transunion/transunion';
-import { Observable, Subscription } from 'rxjs';
-import { DisputeReconfirmFilter } from '@views/dashboard/disputes/disputes-reconfirm/types/dispute-reconfirm-filters';
 import { ROUTE_NAMES as routes } from '@shared/routes/routes.names';
-import { InterstitialService } from '@shared/services/interstitial/interstitial.service';
-import { NegativeAccountService } from '@views/dashboard/negative-account/negative-account-service/negative-account-service.service';
 import { AccountService } from '@shared/services/account/account.service';
-import { MergereportToNegativeTradelinesPipe } from '@shared/pipes/mergereport-to-negative-tradelines/mergereport-to-negative-tradelines.pipe';
 
 @Component({
   selector: 'brave-negative-account-initial',
   templateUrl: './negative-account-initial.component.html',
 })
 export class NegativeAccountInitialComponent implements OnInit {
-  creditReport$: Subscription;
   report: IMergeReport | undefined;
-  _acknowledged: boolean = false;
 
   constructor(
     private router: Router,
-    private statesvc: StateService,
-    private creditReportService: CreditreportService,
-    public negAccService: NegativeAccountService,
     public account: AccountService,
-    private mergePipe: MergereportToNegativeTradelinesPipe,
   ) {
-    this.creditReport$ = this.creditReportService.tuReport$.asObservable().subscribe((v) => {
-      this.report = v;
-    });
-    this.acknowledged = this.statesvc.state?.appData.agencies?.transunion?.acknowledgedDisputeTerms || false;
-  }
-
-  set acknowledged(value: boolean) {
-    this._acknowledged = value;
-  }
-  get acknowledged(): boolean {
-    return this._acknowledged;
   }
 
   ngOnInit(): void {}
@@ -56,12 +31,5 @@ export class NegativeAccountInitialComponent implements OnInit {
 
   onGoToReportClick(): void {
     this.router.navigate([routes.root.dashboard.report.full]);
-  }
-
-  setAccountData(): void {
-    if (this.report) {
-      this.account.tradeline$.next(this.mergePipe.transform(this.report));
-
-    }
   }
 }
