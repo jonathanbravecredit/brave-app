aa22c9c946b0857d2d3759dfd4f096197ef5a5c4
Jonathan <jonathan@brave.credit>
2022-04-18 19:14:25 -0700

    unit test coverage
    added unit tests for directives

--- src/app/shared/directives/analytics/analytics.directive.spec.ts
+++ src/app/shared/directives/analytics/analytics.directive.spec.ts
@@ -1,8 +1,41 @@
 import { AnalyticsDirective } from './analytics.directive';
+import { Helper } from '@testing/test-helper';
+import { AnalyticClickEvents } from '@shared/services/analytics/analytics/constants';
 
 describe('AnalyticsDirective', () => {
-  xit('should create an instance', () => {
-    // const directive = new AnalyticsDirective();
-    // expect(directive).toBeTruthy();
+  const setup = () => {
+    const serviceMock = jasmine.createSpyObj('AnalyticsService', ['fireClickEvent']);
+    const directive = new AnalyticsDirective(serviceMock);
+    return { directive, serviceMock };
+  };
+  const { directive, serviceMock } = setup();
+  const h = new Helper<AnalyticsDirective>(directive);
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  describe('Properties and methods', () => {
+    it('should have a property called analyticClickEvents and set to default', () => {
+      expect(h.hasProperty(directive, 'analyticClickEvents')).toEqual(true);
+      expect(directive.analyticClickEvents).toEqual(AnalyticClickEvents.UnknownClickEvent);
+    });
+    it('should have a property called analyticConfig and set to default', () => {
+      expect(h.hasProperty(directive, 'analyticConfig')).toEqual(true);
+      expect(directive.analyticConfig).toEqual({ google: true, mixpanel: true, brave: false });
+    });
+    it('should have a method called onClick', () => {
+      expect(h.hasMethod(directive, 'onClick')).toEqual(true);
+    });
+  });
+
+  describe('onClick method', () => {
+    it('should call analytics.fireClickEvent', () => {
+      directive.onClick();
+      expect(serviceMock.fireClickEvent).toHaveBeenCalledWith(AnalyticClickEvents.UnknownClickEvent, {
+        google: true,
+        mixpanel: true,
+        brave: false,
+      });
+    });
   });
 });
