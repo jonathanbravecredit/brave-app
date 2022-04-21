e8cba21bece939ab082109b86470c7c306b42339
NCHitsman <noah.hitsman@gmail.com>
2022-04-21 15:38:57 -0700

    tests

--- src/app/shared/resolvers/credit-report/credit-report.resolver.spec.ts
+++ src/app/shared/resolvers/credit-report/credit-report.resolver.spec.ts
@@ -22,93 +22,70 @@ describe('CreditReportResolver', () => {
     expect(creditReportResolver).toBeTruthy();
   });
 
-  // it('should run store.selectOnce when resolve is called', () => {
-  //   creditReportResolver.resolve();
+  it('should run store.selectOnce when resolve is called', () => {
+    creditReportResolver.resolve();
+    expect(storeMock.selectOnce).toHaveBeenCalled();
+  });
 
-  //   expect(storeMock.selectOnce).toHaveBeenCalled();
-  // });
+  it('should run store.selectOnce when resolve is called', fakeAsync(() => {
+    storeMock.selectOnce.and.returnValue({ toPromise: () => Promise.resolve({} as CreditReportStateModel) });
+    let spy = spyOn(creditReportResolver, 'isFresh');
+    spy.and.returnValue(Promise.resolve(true))
+    creditReportResolver.resolve();
+    tick();
+    expect(creditReportResolver.isFresh).toHaveBeenCalled();
+  }));
 
-  // it('should run store.selectOnce when resolve is called', fakeAsync(() => {
-  //   storeMock.selectOnce.and.returnValue({ toPromise: () => Promise.resolve({} as CreditReportStateModel) });
+  it('resolve should return state.report if fresh is truthy', fakeAsync(() => {
+    const test = { TrueLinkCreditReportType: {} as ITrueLinkCreditReportType };
+    storeMock.selectOnce.and.returnValue({
+      toPromise: () => Promise.resolve({ report: test, updatedOn: new Date().toISOString() } as CreditReportStateModel),
+    });
+    let res: any;
+    creditReportResolver.resolve().then((v) => (res = v));
+    tick();
+    expect(res).toEqual(test);
+  }));
 
-  //   let spy = spyOn(creditReportResolver, 'isFresh');
+  it('resolve should run getCurrentCreditReport if fresh is false', fakeAsync(() => {
+    storeMock.selectOnce.and.returnValue({
+      toPromise: () => Promise.resolve({ report: {}, updatedOn: null } as CreditReportStateModel),
+    });
+    creditReportResolver.resolve();
+    tick();
+    expect(creditReportMock.getCurrentCreditReport).toHaveBeenCalled();
+  }));
 
-  //   spy.and.returnValue(Promise.resolve(true))
+  it('resolve should run setCreditReport if fresh is false', fakeAsync(() => {
+    spyOn(creditReportResolver, 'setCreditReport').and.returnValue(Promise.resolve());
+    creditReportMock.getCurrentCreditReport.and.returnValue(Promise.resolve({} as ICreditReport));
+    storeMock.selectOnce.and.returnValue({
+      toPromise: () => Promise.resolve({ report: {}, updatedOn: null } as CreditReportStateModel),
+    });
+    creditReportResolver.resolve();
+    tick();
+    expect(creditReportResolver.setCreditReport).toHaveBeenCalled();
+  }));
 
-  //   creditReportResolver.resolve();
+  it('should run creditReportV2.updateCreditReportStateAsync when setCreditReport is called', () => {
+    creditReportResolver.setCreditReport({} as ICreditReport);
+    expect(creditReportMock.updateCreditReportStateAsync).toHaveBeenCalled();
+  });
 
-  //   tick();
+  it('should return false if report or updated on are null when isFresh is called', async () => {
+    let res = await creditReportResolver.isFresh({ report: null } as CreditReportStateModel);
+    expect(res).toBeFalse();
+  });
 
-  //   expect(creditReportResolver.isFresh).toHaveBeenCalled();
-  // }));
+  it('should return false if report or updated on are not null and updated on is 24 hours older than now when isFresh is called', async () => {
+    let date = dayjs(new Date()).subtract(7, 'day').toISOString();
+    let res = await creditReportResolver.isFresh({ report: {}, updatedOn: date } as CreditReportStateModel);
+    expect(res).toBeFalse();
+  });
 
-  // it('resolve should return state.report if fresh is truthy', fakeAsync(() => {
-  //   const test = { TrueLinkCreditReportType: {} as ITrueLinkCreditReportType };
-
-  //   storeMock.selectOnce.and.returnValue({
-  //     toPromise: () => Promise.resolve({ report: test, updatedOn: new Date().toISOString() } as CreditReportStateModel),
-  //   });
-
-  //   let res: any;
-
-  //   creditReportResolver.resolve().then((v) => (res = v));
-
-  //   tick();
-
-  //   expect(res).toEqual(test);
-  // }));
-
-  // it('resolve should run getCurrentCreditReport if fresh is false', fakeAsync(() => {
-  //   storeMock.selectOnce.and.returnValue({
-  //     toPromise: () => Promise.resolve({ report: {}, updatedOn: null } as CreditReportStateModel),
-  //   });
-
-  //   creditReportResolver.resolve();
-
-  //   tick();
-
-  //   expect(creditReportMock.getCurrentCreditReport).toHaveBeenCalled();
-  // }));
-
-  // it('resolve should run setCreditReport if fresh is false', fakeAsync(() => {
-  //   spyOn(creditReportResolver, 'setCreditReport').and.returnValue(Promise.resolve());
-
-  //   creditReportMock.getCurrentCreditReport.and.returnValue(Promise.resolve({} as ICreditReport));
-
-  //   storeMock.selectOnce.and.returnValue({
-  //     toPromise: () => Promise.resolve({ report: {}, updatedOn: null } as CreditReportStateModel),
-  //   });
-
-  //   creditReportResolver.resolve();
-
-  //   tick();
-
-  //   expect(creditReportResolver.setCreditReport).toHaveBeenCalled();
-  // }));
-
-  // it('should run creditReportV2.updateCreditReportStateAsync when setCreditReport is called', () => {
-  //   creditReportResolver.setCreditReport({} as ICreditReport);
-
-  //   expect(creditReportMock.updateCreditReportStateAsync).toHaveBeenCalled();
-  // });
-
-  // it('should return false if report or updated on are null when isFresh is called', async () => {
-  //   let res = await creditReportResolver.isFresh({ report: null } as CreditReportStateModel);
-
-  //   expect(res).toBeFalse();
-  // });
-
-  // it('should return false if report or updated on are not null and updated on is 24 hours older than now when isFresh is called', async () => {
-  //   let date = dayjs(new Date()).subtract(7, 'day').toISOString();
-  //   let res = await creditReportResolver.isFresh({ report: {}, updatedOn: date } as CreditReportStateModel);
-
-  //   expect(res).toBeFalse();
-  // });
-
-  // it('should return true if report or updated on are not null and updated on is not 24 hours older than now when isFresh is called', async () => {
-  //   let date = new Date().toISOString();
-  //   let res = await creditReportResolver.isFresh({ report: {}, updatedOn: date } as CreditReportStateModel);
-
-  //   expect(res).toBeTrue();
-  // });
+  it('should return true if report or updated on are not null and updated on is not 24 hours older than now when isFresh is called', async () => {
+    let date = new Date().toISOString();
+    let res = await creditReportResolver.isFresh({ report: {}, updatedOn: date } as CreditReportStateModel);
+    expect(res).toBeTrue();
+  });
 });
