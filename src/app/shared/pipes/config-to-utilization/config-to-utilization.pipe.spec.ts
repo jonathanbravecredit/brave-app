abcdbb7e83db71e040f39005bda2f92e6bd4e85a
NCHitsman <noah.hitsman@gmail.com>
2022-04-26 12:22:55 -0700

    tests

--- src/app/shared/pipes/config-to-utilization/config-to-utilization.pipe.spec.ts
+++ src/app/shared/pipes/config-to-utilization/config-to-utilization.pipe.spec.ts
@@ -1,8 +1,37 @@
+import { ICreditUtilization } from "@views/dashboard/credit-utilization/components/credit-utilization-card/interfaces";
 import { ConfigToUtilizationPipe } from "./config-to-utilization.pipe";
 
 describe("ConfigToUtilizationPipe", () => {
+  let pipe: any;
+  beforeAll(() => {
+    pipe = new ConfigToUtilizationPipe();
+  });
+
   it("create an instance", () => {
-    const pipe = new ConfigToUtilizationPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it("should return undefined if value is undefined on transform", () => {
+    let res = pipe.transform(null);
+    expect(res).toBeUndefined();
+  });
+
+  it("should return expect if value is a ICreditUtilization on transform", () => {
+    let mock = {
+      creditorName: "test1",
+      accountName: "test2",
+      currentBalance: "test3",
+      creditLimit: "test4",
+      openClosed: "test5",
+    } as ICreditUtilization;
+    let res = pipe.transform(mock);
+    expect(res).toEqual({
+      config: mock,
+      creditorName: "test1",
+      accountName: "test1",
+      currentBalance: "test3",
+      creditLimit: "test4",
+      openClosed: "test5",
+    });
   });
 });
--- src/app/shared/pipes/disputes-to-disputes-historical/disputes-to-disputes-historical.pipe.spec.ts
+++ src/app/shared/pipes/disputes-to-disputes-historical/disputes-to-disputes-historical.pipe.spec.ts
@@ -1,8 +1,26 @@
-import { DisputesToDisputesHistoricalPipe } from './disputes-to-disputes-historical.pipe';
+import { IDispute } from "@shared/interfaces/disputes";
+import { IDisputeHistorical } from "@views/dashboard/disputes/components/cards";
+import { DisputesToDisputesHistoricalPipe } from "./disputes-to-disputes-historical.pipe";
 
-describe('DisputesToDisputesHistoricalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DisputesToDisputesHistoricalPipe();
+describe("DisputesToDisputesHistoricalPipe", () => {
+  let pipe: any;
+  beforeAll(() => {
+    pipe = new DisputesToDisputesHistoricalPipe();
+  });
+
+  it("create an instance", () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it("should return Undefined on transform if no dispute", () => {
+    let res = pipe.transform(null, "");
+    expect(res).toEqual([]);
+  });
+
+  it("should return expected on transform if dispute", () => {
+    let spy = spyOn(pipe, "parseHistoricalDisputeItems");
+    spy.and.returnValue({} as IDisputeHistorical);
+    let res = pipe.transform([true, { disputeItems: "" } as IDispute]);
+    expect(res).toEqual([]);
   });
 });
