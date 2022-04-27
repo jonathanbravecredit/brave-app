1c8881d22b6dbc0856442c928cb77be078d9d6cb
NCHitsman <noah.hitsman@gmail.com>
2022-04-27 15:44:41 -0700

    wip

--- src/app/shared/services/progress-tracker/progress-tracker-service.service.ts
+++ src/app/shared/services/progress-tracker/progress-tracker-service.service.ts
@@ -1,38 +1,45 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable, OnDestroy } from '@angular/core';
-import { AuthService } from '@shared/services/auth/auth.service';
-import { environment } from '@environments/environment';
-import { IGoalInfo } from '@views/onboarding/kyc-goal-choice/kyc-goal-choice/kyc-goal-choice.component';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable, OnDestroy } from "@angular/core";
+import { AuthService } from "@shared/services/auth/auth.service";
+import { environment } from "@environments/environment";
+import { IGoalInfo } from "@views/onboarding/kyc-goal-choice/kyc-goal-choice/kyc-goal-choice.component";
 import {
   Initiative,
   InitiativePatchBody,
   InitiativeSubTask,
   InitiativeTask,
-} from '@shared/interfaces/progress-tracker.interface';
-import * as ProgressTrackerActions from '@store/progress-tracker/progress-tracker.actions';
-import { Select, Store } from '@ngxs/store';
-import { ProgressTrackerState, ProgressTrackerStateModel } from '@store/progress-tracker';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { ICircleProgressStep } from '@shared/components/progressbars/circle-checktext-progressbar/circle-checktext-progressbar';
-import { TransunionInput } from '@shared/services/aws/api.service';
+} from "@shared/interfaces/progress-tracker.interface";
+import * as ProgressTrackerActions from "@store/progress-tracker/progress-tracker.actions";
+import { Select, Store } from "@ngxs/store";
+import {
+  ProgressTrackerState,
+  ProgressTrackerStateModel,
+} from "@store/progress-tracker";
+import { BehaviorSubject, Observable, Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
+import { ICircleProgressStep } from "@shared/components/progressbars/circle-checktext-progressbar/circle-checktext-progressbar";
+import { TransunionInput } from "@shared/services/aws/api.service";
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class ProgressTrackerService implements OnDestroy {
-  @Select(ProgressTrackerState) initiative$!: Observable<ProgressTrackerStateModel>;
+  @Select(ProgressTrackerState)
+  initiative$!: Observable<ProgressTrackerStateModel>;
   initiativeSub$: Subscription | undefined;
   initiative: Initiative | null = null;
   initiativeSteps: ICircleProgressStep[] = [];
-  initiativeSteps$: BehaviorSubject<ICircleProgressStep[]> = new BehaviorSubject<ICircleProgressStep[]>([]);
+  initiativeSteps$: BehaviorSubject<ICircleProgressStep[]> =
+    new BehaviorSubject<ICircleProgressStep[]>([]);
 
   enrolledOn: string | null | undefined;
   enrolledScore: string | null | undefined;
   currentScore: string | null | undefined;
   storeSub$: Subscription | undefined;
 
-  constructor(private http: HttpClient, private auth: AuthService, private store: Store) {
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private store: Store
+  ) {
     this.subscribeToProgressTrackerData();
     this.subscribeToStoreValues();
   }
@@ -43,13 +50,15 @@ export class ProgressTrackerService implements OnDestroy {
   }
 
   subscribeToProgressTrackerData(): void {
-    this.initiativeSub$ = this.initiative$.pipe(filter((res) => res !== undefined)).subscribe((res) => {
-      if (res.data) {
-        this.createInitiativeSteps(res.data);
-        this.initiativeSteps$.next(this.initiativeSteps);
-      }
-      this.initiative = res.data;
-    });
+    this.initiativeSub$ = this.initiative$
+      .pipe(filter((res) => res !== undefined))
+      .subscribe((res) => {
+        if (res.data) {
+          this.createInitiativeSteps(res.data);
+          this.initiativeSteps$.next(this.initiativeSteps);
+        }
+        this.initiative = res.data;
+      });
   }
 
   subscribeToStoreValues(): void {
@@ -79,19 +88,22 @@ export class ProgressTrackerService implements OnDestroy {
     const initTaskLen = initiative?.initiativeTasks?.length || 0;
     const tasks = initiative?.initiativeTasks || [];
     const subTasks = initiative?.initiativeTasks[0].subTasks || [];
-    this.initiativeSteps = initTaskLen > 1 ? tasks.map(mapTask) : subTasks.map(mapTask);
+    this.initiativeSteps =
+      initTaskLen > 1 ? tasks.map(mapTask) : subTasks.map(mapTask);
   }
 
   updateSteps(task: InitiativeSubTask | InitiativeTask): void {
     this.initiativeSteps.forEach((step) => {
       if ((step.stepId = task.taskId)) {
-        step.stepComplete = task.taskStatus === 'complete';
+        step.stepComplete = task.taskStatus === "complete";
       }
     });
   }
 
   updateProgressTrackerState(progressTrackerData: Initiative) {
-    this.store.dispatch(new ProgressTrackerActions.Add({ data: progressTrackerData }));
+    this.store.dispatch(
+      new ProgressTrackerActions.Add({ data: progressTrackerData })
+    );
   }
 
   async getProgressTrackerData(): Promise<Initiative | null> {
@@ -101,7 +113,9 @@ export class ProgressTrackerService implements OnDestroy {
     });
 
     try {
-      let res = await this.http.get<Initiative>(environment.api + '/initiatives', { headers }).toPromise();
+      let res = await this.http
+        .get<Initiative>(environment.api + "/initiatives", { headers })
+        .toPromise();
       this.updateProgressTrackerState(res);
       return res;
     } catch (error) {
@@ -109,14 +123,20 @@ export class ProgressTrackerService implements OnDestroy {
     }
   }
 
-  async updateProgressTrackerData(patchBody: InitiativePatchBody): Promise<Initiative | null> {
+  async updateProgressTrackerData(
+    patchBody: InitiativePatchBody
+  ): Promise<Initiative | null> {
     const token = await this.auth.getIdTokenJwtTokens();
     const headers = new HttpHeaders({
       Authorization: `${token}`,
     });
 
     try {
-      let res = await this.http.put<Initiative>(environment.api + '/initiatives', patchBody, { headers }).toPromise(); //!change test url
+      let res = await this.http
+        .put<Initiative>(environment.api + "/initiatives", patchBody, {
+          headers,
+        })
+        .toPromise(); //!change test url
       await this.getProgressTrackerData();
       return res;
     } catch (err) {
@@ -134,7 +154,11 @@ export class ProgressTrackerService implements OnDestroy {
       reason: goalInfo.reason,
     };
 
-    return await this.http.post<Initiative>(environment.api + '/initiatives', patchBody, { headers }).toPromise();
+    return await this.http
+      .post<Initiative>(environment.api + "/initiatives", patchBody, {
+        headers,
+      })
+      .toPromise();
   }
 
   async postThenGetUserGoal(goalInfo: IGoalInfo) {
@@ -146,10 +170,16 @@ export class ProgressTrackerService implements OnDestroy {
       programId: goalInfo.programId,
       reason: goalInfo.reason,
     };
-    await this.http.post<Initiative>(environment.api + '/initiatives', patchBody, { headers }).toPromise();
-    let res = await this.http.get<Initiative>(environment.api + '/initiatives', { headers }).toPromise();
+    await this.http
+      .post<Initiative>(environment.api + "/initiatives", patchBody, {
+        headers,
+      })
+      .toPromise();
+    let res = await this.http
+      .get<Initiative>(environment.api + "/initiatives", { headers })
+      .toPromise();
     this.updateProgressTrackerState(res);
-    return res
+    return res;
   }
 }
 
@@ -159,7 +189,7 @@ const mapTask = (task: InitiativeTask | InitiativeSubTask, i: number) => {
     id: i,
     stepId: taskId,
     stepActive: true,
-    stepComplete: taskStatus === 'complete',
+    stepComplete: taskStatus === "complete",
     stepLabel: taskLabel,
   };
 };
---
+++ src/app/views/dashboard/progress-tracker/progress-tracker.content.ts
@@ -0,0 +1,17 @@
+export const NEGATIVE_ACCOUNT_CONTENT = {
+    negativeAccountInitial: {
+      hasNegativeAccounts: {
+        h2: `New Negative Accounts`,
+        p1: `High Score Impact`,
+        p2: `These are new negative accounts in the last 30 days that are in collections, have been charged off, or have late payments.`,
+        p3: `Review the accounts for accuracy, and if you see an error, you can dispute them.`,
+      },
+      noNegativeAccounts: {
+        p1: `No new negative accounts in the last 30 days!`,
+        p2: `To see older negative accounts, visit your credit report.`,
+        b1: `View Full Report`,
+        b2: `Return to Dashboard`,
+      },
+    },
+  };
+  
