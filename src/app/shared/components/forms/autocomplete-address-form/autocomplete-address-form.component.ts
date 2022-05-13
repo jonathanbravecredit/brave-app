47f0d9121a23f79814e202de3cc03afde8255a2d
Jonathan <jonathan@brave.credit>
2022-05-12 17:28:13 -0700

    injecting state validator to autocomplete

--- src/app/shared/components/forms/autocomplete-address-form/autocomplete-address-form.component.ts
+++ src/app/shared/components/forms/autocomplete-address-form/autocomplete-address-form.component.ts
@@ -1,61 +1,57 @@
-import { Component, ViewChild } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
-import { BaseFormComponent } from "@shared/components/forms/base-form/base-form.component";
-import { OutlineInputComponent } from "@shared/components/inputs/outline-input/outline-input.component";
-import { Observable } from "rxjs";
-import { FilledSpinningButtonComponent } from "../../buttons/filled-spinning-button/filled-spinning-button.component";
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, ValidatorFn } from '@angular/forms';
+import { BaseFormComponent } from '@shared/components/forms/base-form/base-form.component';
+import { stateValidator } from '@shared/components/forms/outline-address-form/outline-address-form.validators';
+import { OutlineInputComponent } from '@shared/components/inputs/outline-input/outline-input.component';
+import { Observable } from 'rxjs';
+import { FilledSpinningButtonComponent } from '../../buttons/filled-spinning-button/filled-spinning-button.component';
 // DO NOT REMOVE BELOW REFERENCE!!!!
 /// <reference types="google.maps" />
 declare var google: any;
 // DO NOT REMOVE ABOVE REFERENCE!!!!
 
 @Component({
-  selector: "brave-autocomplete-address-form",
-  templateUrl: "./autocomplete-address-form.component.html",
+  selector: 'brave-autocomplete-address-form',
+  templateUrl: './autocomplete-address-form.component.html',
 })
 export class AutocompleteAddressFormComponent extends BaseFormComponent {
-  @ViewChild(OutlineInputComponent) addressTwoInput:
-    | OutlineInputComponent
-    | undefined;
-  @ViewChild("spinner") spinner: FilledSpinningButtonComponent | undefined;
+  @ViewChild(OutlineInputComponent) addressTwoInput: OutlineInputComponent | undefined;
+  @ViewChild('spinner') spinner: FilledSpinningButtonComponent | undefined;
+
   values$: Observable<any>;
   status$: Observable<any>;
 
   constructor(fb: FormBuilder) {
-    super(fb, "address-form");
+    super(fb, 'address-form', [stateValidator]);
     this.values$ = this.parentForm.valueChanges;
     this.status$ = this.parentForm.statusChanges;
   }
 
   onAddressClick(place: google.maps.places.PlaceResult): void {
-    let addressOne = "";
+    let addressOne = '';
     let addressTwo;
-    let city = "";
-    let state = "";
-    let zip = "";
+    let city = '';
+    let state = '';
+    let zip = '';
     if (!place?.address_components || !place?.address_components.length) return;
     for (const component of place.address_components as unknown as google.maps.GeocoderAddressComponent[]) {
       // @ts-ignore remove once typings fixed
       const componentType = component.types[0];
 
       switch (componentType) {
-        case "street_number":
-          addressOne = addressOne
-            ? `${component.long_name} ${addressOne}`
-            : component.long_name;
+        case 'street_number':
+          addressOne = addressOne ? `${component.long_name} ${addressOne}` : component.long_name;
           break;
-        case "route":
-          addressOne = addressOne
-            ? `${addressOne} ${component.short_name}`
-            : component.short_name;
+        case 'route':
+          addressOne = addressOne ? `${addressOne} ${component.short_name}` : component.short_name;
           break;
-        case "postal_code":
+        case 'postal_code':
           zip = `${component.long_name}`;
           break;
-        case "locality":
+        case 'locality':
           city = component.long_name;
           break;
-        case "administrative_area_level_1":
+        case 'administrative_area_level_1':
           state = component.short_name;
           break;
       }
