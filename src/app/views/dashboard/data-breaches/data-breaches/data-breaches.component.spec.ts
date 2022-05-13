ee3847089ed5fe0a0fae8b992ccbfcf1e2c61864
Jonathan <jonathan@brave.credit>
2022-05-13 10:46:53 -0700

    fixing build errors

--- src/app/views/dashboard/data-breaches/data-breaches/data-breaches.component.spec.ts
+++ src/app/views/dashboard/data-breaches/data-breaches/data-breaches.component.spec.ts
@@ -2,12 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { APIService } from '@shared/services/aws/api.service';
 import { StateService } from '@shared/services/state/state.service';
+import { DataBreachesComponent } from '@views/dashboard/data-breaches/data-breaches/data-breaches.component';
 import { Observable, of } from 'rxjs';
 
 //   private route: ActivatedRoute,
 //   public dataBreachesViewService: DataBreachesViewService
 
-describe("DataBreachesComponent", () => {
+describe('DataBreachesComponent', () => {
   let component: DataBreachesComponent;
   let fixture: ComponentFixture<DataBreachesComponent>;
   let stateMock: any;
@@ -35,7 +36,7 @@ describe("DataBreachesComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
--- src/app/views/dashboard/data-breaches/data-breaches/data-breaches.component.ts
+++ src/app/views/dashboard/data-breaches/data-breaches/data-breaches.component.ts
@@ -4,17 +4,22 @@ import { AnalyticClickEvents } from '@shared/services/analytics/analytics/consta
 import { IBreachCard } from '@shared/interfaces/breach-card.interface';
 import { StateService } from '@shared/services/state/state.service';
 import * as DashboardActions from '@store/dashboard/dashboard.actions';
+import { DataBreachesViewService } from '@views/dashboard/data-breaches/data-breaches-view.service';
 
 @Component({
-  selector: "brave-data-breaches",
-  templateUrl: "./data-breaches.component.html",
+  selector: 'brave-data-breaches',
+  templateUrl: './data-breaches.component.html',
 })
 export class DataBreachesComponent implements OnInit {
   breaches: IBreachCard[] | undefined;
   AnalyticClickEvents = AnalyticClickEvents;
-  constructor(private route: ActivatedRoute, private state: StateService) {
+  constructor(
+    private route: ActivatedRoute,
+    private state: StateService,
+    private dataBreachesViewService: DataBreachesViewService,
+  ) {
     this.route.data.subscribe((resp: any) => {
-      this.dataBreachesViewService.initialModelMerge(resp.breaches)
+      this.dataBreachesViewService.initialModelMerge(resp.breaches);
     });
   }
 
