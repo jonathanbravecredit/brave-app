780828edd97c94abd4164e0f76acdab1cd267336
Jonathan <jonathan@brave.credit>
2022-05-18 11:07:39 -0700

    view details new application
    updating views with static version of view details
    updating dashboard enrolled
    updating credit reports
    updating forbearance 
    updating tailwind to include the brighten class

--- src/app/views/dashboard/reports/credit-report/publicitems/components/publicitem-card/publicitem-card.component.ts
+++ src/app/views/dashboard/reports/credit-report/publicitems/components/publicitem-card/publicitem-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ViewDetailOrientation } from '@shared/components/buttons/viewdetail-button/viewdetail-button.component';
 import { IPublicPartition } from '@shared/interfaces';
 
 export type PublicItemCardFieldType = 'string' | 'currency' | 'date';
@@ -13,6 +14,7 @@ export class PublicitemCardComponent implements OnInit {
   @Input() firstFieldValue: string = '';
   @Input() firstFieldType: PublicItemCardFieldType = 'string';
   @Input() publicItem: IPublicPartition = {} as IPublicPartition; // bring the unmapped public item foreward
+  @Input() viewDetailOrientation: ViewDetailOrientation = 'horizontal-right';
   @Output() viewDetailClick: EventEmitter<void> = new EventEmitter();
   constructor() {}
 
