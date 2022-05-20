77c1f9d8061accad5a2e3c9feba444392d606c33
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2022-05-20 12:04:14 -0700

    fin

--- src/app/views/dashboard/reports/credit-report/tradelines/components/tradeline-summary/tradeline-summary.component.ts
+++ src/app/views/dashboard/reports/credit-report/tradelines/components/tradeline-summary/tradeline-summary.component.ts
@@ -1,28 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ITradeLinePartition } from "../../../../../../../shared/interfaces/merge-report.interface";
 
 @Component({
-  selector: 'brave-tradeline-summary',
-  templateUrl: './tradeline-summary.component.html',
+  selector: "brave-tradeline-summary",
+  templateUrl: "./tradeline-summary.component.html",
 })
-export class TradelineSummaryComponent {
+export class TradelineSummaryComponent implements OnInit {
   /**
    * Raw payment status from Merge Report
    * @property {string} status
    * @default
    */
-  @Input() status: string = '';
+  @Input() status: string = "";
   /**
    * Raw creditor name from Merge Report
    * @property {string} creditorName
    * @default
    */
-  @Input() creditorName: string = '';
+  @Input() creditorName: string = "";
   /**
    * Raw date reported from Merge Report
    * @property {string} dateReported
    * @default
    */
-  @Input() dateReported: string = '';
+  @Input() dateReported: string = "";
   /**
    * Raw current balance from Merge Report
    * @property {number | string} currentBalance
@@ -34,7 +35,17 @@ export class TradelineSummaryComponent {
    * @property {string} openClosed
    * @default
    */
-  @Input() openClosed: string = '';
+  @Input() openClosed: string = "";
+
+  @Input() tradeline: ITradeLinePartition | undefined | null =
+    {} as ITradeLinePartition;
+
+  payStatusSymbol: string | undefined;
 
   constructor() {}
+
+  ngOnInit() {
+    this.payStatusSymbol =
+      this.tradeline?.Tradeline?.PayStatus?.symbol?.toString();
+  }
 }
