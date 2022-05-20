20f0cf3d6aba38290017bb7fd24e6b9adf112e06
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2022-05-20 12:43:11 -0700

    requested changes

--- src/app/views/dashboard/reports/credit-report/tradelines/components/tradeline-summary/tradeline-summary.component.ts
+++ src/app/views/dashboard/reports/credit-report/tradelines/components/tradeline-summary/tradeline-summary.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "brave-tradeline-summary",
   templateUrl: "./tradeline-summary.component.html",
 })
-export class TradelineSummaryComponent {
+export class TradelineSummaryComponent implements OnInit {
   /**
    * Raw payment status from Merge Report
    * @property {string} status
@@ -38,5 +38,42 @@ export class TradelineSummaryComponent {
 
   @Input() payStatusSymbol: string | undefined;
 
+  isOpen: boolean = false;
+
+  get closed(): boolean {
+    return !this.isOpen;
+  }
+
+  get safe(): boolean {
+    return this.isOpen && this.payStatusSymbol?.toLowerCase() === "c";
+  }
+
+  get danger(): boolean {
+    return (
+      this.payStatusSymbol === "1" ||
+      this.payStatusSymbol === "2" ||
+      this.payStatusSymbol === "3" ||
+      this.payStatusSymbol === "4" ||
+      this.payStatusSymbol === "7" ||
+      this.payStatusSymbol === "8R"
+    );
+  }
+
+  get critical(): boolean {
+    return this.payStatusSymbol === "9";
+  }
+
+  get default(): boolean {
+    if (!this.closed && !this.safe && !this.danger && !this.critical) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   constructor() {}
+
+  ngOnInit() {
+    this.isOpen = this.openClosed.toLowerCase() === "o";
+  }
 }
